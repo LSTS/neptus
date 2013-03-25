ce7bb9f3b68d1e55716ee9010d2661ebbdd67994
Margarida <meg@Helper>
2013-03-25 15:50:07 +0000

    upload of part of the project SpotUpdaters code and first commit of feature plugins code

---
+++ plugins-dev/spot/pt/up/fe/dceg/neptus/plugins/spot/SpotOverlay.java
@@ -0,0 +1,113 @@
+/*
+ * Copyright (c) 2004-2013 Universidade do Porto - Faculdade de Engenharia
+ * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * All rights reserved.
+ * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ *
+ * This file is part of Neptus, Command and Control Framework.
+ *
+ * Commercial Licence Usage
+ * Licencees holding valid commercial Neptus licences may use this file
+ * in accordance with the commercial licence agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in a
+ * written agreement between you and Universidade do Porto. For licensing
+ * terms, conditions, and further information contact lsts@fe.up.pt.
+ *
+ * European Union Public Licence - EUPL v.1.1 Usage
+ * Alternatively, this file may be used under the terms of the EUPL,
+ * Version 1.1 only (the "Licence"), appearing in the file LICENSE.md
+ * included in the packaging of this file. You may not use this work
+ * except in compliance with the Licence. Unless required by applicable
+ * law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific
+ * language governing permissions and limitations at
+ * https://www.lsts.pt/neptus/licence.
+ *
+ * For more information please see <http://lsts.fe.up.pt/neptus>.
+ *
+ * Author: Margarida Faria
+ * Mar 25, 2013
+ */
+package pt.up.fe.dceg.neptus.plugins.spot;
+
+import java.io.IOException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.xml.sax.SAXException;
+
+import pt.up.fe.dceg.neptus.console.ConsoleLayout;
+import pt.up.fe.dceg.neptus.plugins.PluginDescription;
+import pt.up.fe.dceg.neptus.plugins.SimpleRendererInteraction;
+import pt.up.fe.dceg.neptus.plugins.update.IPeriodicUpdates;
+
+/**
+ * @author Margarida Faria
+ *
+ */
+@PluginDescription(author = "Margarida", name = "SPOT Overlay")
+public class SpotOverlay extends SimpleRendererInteraction implements IPeriodicUpdates {
+    private static final long serialVersionUID = -4807939956933128721L;
+    private final int updateMillis;
+    private final String getUrl;
+    private final String postUrl;
+    private SpotUpdater spotUpdater;
+    /**
+     * @param console
+     */
+    public SpotOverlay(ConsoleLayout console) {
+        super(console);
+        getUrl = "http://tiny.cc/spot1";
+        postUrl = "http://whale.fe.up.pt/neptleaves/state";
+        updateMillis = 60000;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see pt.up.fe.dceg.neptus.renderer2d.StateRendererInteraction#isExclusive()
+     */
+    @Override
+    public boolean isExclusive() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public long millisBetweenUpdates() {
+        return updateMillis;
+    }
+
+    @Override
+    public boolean update() {
+        try {
+            spotUpdater.update(getUrl, postUrl);
+        }
+        catch (ParserConfigurationException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (SAXException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void initSubPanel() {
+        spotUpdater = new SpotUpdater();
+    }
+
+    @Override
+    public void cleanSubPanel() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ plugins-dev/spot/pt/up/fe/dceg/neptus/plugins/spot/SpotUpdater.java
@@ -0,0 +1,115 @@
+package pt.up.fe.dceg.neptus.plugins.spot;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Collections;
+import java.util.Vector;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+public class SpotUpdater {
+
+
+    public static Vector<SpotMessage> get(String url) throws ParserConfigurationException, SAXException, IOException {
+
+		Vector<SpotMessage> updates = new Vector<SpotMessage>();
+
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+		DocumentBuilder db = dbf.newDocumentBuilder();
+		Document doc = db.parse(url);
+		NodeList nlist = doc.getFirstChild().getChildNodes();
+		
+		for (int i = 1; i < nlist.getLength(); i++) {
+			String tagName = nlist.item(i).getNodeName();
+			if (tagName.equals("message")) {
+				double lat = 0,lon = 0;
+				String id = "SPOT";
+				long timestamp = System.currentTimeMillis();
+
+				NodeList elems = nlist.item(i).getChildNodes();
+				for (int j = 0; j < elems.getLength(); j++) {
+					String tag = elems.item(j).getNodeName();
+
+					if (tag.equals("latitude"))
+						lat = Double.parseDouble(elems.item(j).getTextContent());
+					else if (tag.equals("longitude"))
+						lon = Double.parseDouble(elems.item(j).getTextContent());
+					else if (tag.equals("esnName"))
+						id = elems.item(j).getTextContent();
+					else if (tag.equals("timeInGMTSecond")) {
+						timestamp = Long.parseLong(elems.item(j).getTextContent());
+						timestamp *= 1000; //secs to millis
+					}
+				}
+				updates.add(new SpotMessage(lat, lon, timestamp, id));
+			}			
+		}
+		return updates;
+	}
+
+    public static String post(SpotMessage update, String postUrl) throws IOException {
+
+		String xml = "<MissionState>\n"+update.asVehicleState()+"</MissionState>\n";
+
+		String data = URLEncoder.encode("type", "UTF-8") + "=" + URLEncoder.encode("state", "UTF-8");
+		data += "&" + URLEncoder.encode("xml", "UTF-8") + "=" + URLEncoder.encode(xml, "UTF-8");
+
+		System.out.println("Posting:\n"+xml);
+		
+		// Send data
+		URL url = new URL(postUrl);
+		URLConnection conn = url.openConnection();
+		conn.setDoOutput(true);
+		OutputStreamWriter wr = new OutputStreamWriter(conn.getOutputStream());
+		wr.write(data);
+		wr.flush();
+
+		// Get the response
+		BufferedReader rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+		String line;
+		String resp = "Response: \n";
+		while ((line = rd.readLine()) != null) {
+			resp += line;
+		}
+		//Logger.getLogger(SpotUpdater.class.getName()).info(resp);
+
+		wr.close();
+		rd.close();
+		return resp;
+	}
+
+    public static SpotMessage update(String getUrl, String postUrl) throws ParserConfigurationException, SAXException,
+            IOException {
+		Vector<SpotMessage> updates = get(getUrl);
+		if (!updates.isEmpty()) {
+			Collections.sort(updates);
+			SpotMessage mostRecent = updates.lastElement();
+			post(mostRecent, postUrl);
+			return mostRecent;
+		}
+		return null;
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		String getUrl = "http://tiny.cc/spot1";
+		String postUrl = "http://whale.fe.up.pt/neptleaves/state";
+		
+		while(true) {
+			update(getUrl, postUrl);
+			System.out.println("Sleeping for 3 minutes...");
+			Thread.sleep(1000*60*3);
+		}
+	}
+}
