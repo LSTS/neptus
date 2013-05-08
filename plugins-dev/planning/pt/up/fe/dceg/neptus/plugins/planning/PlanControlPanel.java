a33e30155b204250de095d4217e947e4c1f3b886
Margarida Faria <margaridaCostaFaria@gmail.com>
2013-05-08 17:31:45 +0100

    Changing warning message of sending just a group of beacons to a shorter phrase.

--- plugins-dev/planning/pt/up/fe/dceg/neptus/plugins/planning/PlanControlPanel.java
+++ plugins-dev/planning/pt/up/fe/dceg/neptus/plugins/planning/PlanControlPanel.java
@@ -748,7 +748,7 @@ public class PlanControlPanel extends SimpleSubPanel implements ConfigurationLis
                 }
                 int resp = GuiUtils.confirmDialog(SwingUtilities.windowForComponent(this),
                         I18n.text("LBL Beacons"),
-                        I18n.textf("Are you sure that you want to override the existing LBL configuration only with %beaconsToSend?",
+                        I18n.textf("Are you sure you want to send only %beaconsToSend?",
                         beaconsToSend));
                 if (resp == JOptionPane.YES_OPTION) {
                     transpondersList.clear();
