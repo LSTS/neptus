ccedc837320df6246dfd4849c7a0cfbbc136280e
Joao Fortuna <fortuna.araujo@gmail.com>
2013-05-21 19:19:29 +0100

    Display for Ubiquiti AirCams

--- plugins-dev/ipcam/pt/up/fe/dceg/neptus/plugins/ipcam/AirCamDisplay.java
+++ plugins-dev/ipcam/pt/up/fe/dceg/neptus/plugins/ipcam/AirCamDisplay.java
@@ -31,68 +31,222 @@
  */
 package pt.up.fe.dceg.neptus.plugins.ipcam;
 
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 
+import javax.imageio.ImageIO;
+import javax.imageio.stream.ImageInputStream;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.conn.params.ConnManagerParams;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+
+import com.l2fprod.common.propertysheet.DefaultProperty;
+
+import pt.up.fe.dceg.neptus.NeptusLog;
 import pt.up.fe.dceg.neptus.console.ConsoleLayout;
+import pt.up.fe.dceg.neptus.gui.PropertiesEditor;
 import pt.up.fe.dceg.neptus.i18n.I18n;
+import pt.up.fe.dceg.neptus.plugins.ConfigurationListener;
 import pt.up.fe.dceg.neptus.plugins.NeptusProperty;
+import pt.up.fe.dceg.neptus.plugins.PluginDescription;
+import pt.up.fe.dceg.neptus.plugins.Popup;
+import pt.up.fe.dceg.neptus.plugins.SimpleSubPanel;
+import pt.up.fe.dceg.neptus.plugins.Popup.POSITION;
 import pt.up.fe.dceg.neptus.plugins.actions.SimpleMenuAction;
+import pt.up.fe.dceg.neptus.util.GuiUtils;
 
 /**
  * @author jfortuna
  *
  */
-public class AirCamDisplay extends SimpleMenuAction {
+@SuppressWarnings("deprecation")
+@Popup( pos = POSITION.RIGHT, width=400, height=400)
+@PluginDescription(name="AirCam Display", author="JFortuna", description="Video display for Ubiquiti IP Camera", icon="pt/up/fe/dceg/neptus/plugins/ipcam/camera.png")
+public class AirCamDisplay extends SimpleSubPanel implements ConfigurationListener {
 
-    @NeptusProperty(name="Hostname")
-    public String host = "10.0.20.19";
-    /**
-     * @param console
-     */
-    public AirCamDisplay(ConsoleLayout console) {
-        super(console);
-    }
-
-    
-    @Override
-    public String getMenuName() {
-        return I18n.text("Tools")+">"+I18n.text("Connect IP Camera");
-    }
-    
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1L;
 
-    /* (non-Javadoc)
-     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
-     */
+    public enum InterpolationStyle { Nearest_Neighbour, Bilinear, Bicubic }; 
+
+    @NeptusProperty(name="Camera IP", description="The IP address of the camera you want to display")
+    public String ip = "10.0.20.209";
+    
+    @NeptusProperty(name="Milliseconds between connection retries")
+    public long millisBetweenTries = 1000;
+
+    protected BufferedImage imageToDisplay = null;
+    protected boolean connected = true;
+    protected Thread updater = null;
+    protected String status = "initializing";
+
+    public AirCamDisplay(ConsoleLayout console) {
+        super(console);
+        removeAll();
+        
+        status = "initializing...";
+        updater = updaterThread();
+        updater.setPriority(Thread.MIN_PRIORITY);
+        updater.start();
+        
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    JPopupMenu popup = new JPopupMenu();
+                    popup.add("Reconnect").addActionListener(new ActionListener() {
+                        
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            reconnect();
+                        }
+                    });
+                    
+                    popup.add("Camera settings").addActionListener(new ActionListener() {
+                        
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            PropertiesEditor.editProperties(AirCamDisplay.this, getConsole(), true);
+                        }
+                    });
+                    popup.show((Component)e.getSource(), e.getX(), e.getY());
+                }
+                
+            }
+        });
+    }
+
     @Override
-    public void actionPerformed(ActionEvent e) {
-       String option = JOptionPane.showInputDialog(getConsole(), I18n.text("Camera IP"), host);
-       
-       if (option == null)
-           return;
-       host = option;
-       
-       try {
-           Runtime.getRuntime().exec("ffplay rtsp://" + host + ":554/live/ch01_0");
-       }
-       catch (IOException e1) {
-           e1.printStackTrace();
-       }
+    public void paint(Graphics g) {
+        if (imageToDisplay != null) {
+            double factor1 = (getWidth()-1.0) / imageToDisplay.getWidth();
+            double factor2 = (getHeight()-1.0) / imageToDisplay.getHeight();
+            ((Graphics2D)g).scale(factor1, factor2);
+            g.drawImage(imageToDisplay, 0, 0, null);
+        }
+        else {
+            g.setColor(Color.black);
+            g.fillRect(0, 0, getWidth(), getHeight());
+            g.setColor(Color.gray);
+            g.drawRect(0, 0, 320, 240);
+            g.drawRect(0, 0, 640, 480);
+        }
+    }
+
+    @Override
+    public Dimension getPreferredSize() {
+        return new Dimension(320, 240);
+    }
+
+    public void reconnect() {
+        NeptusLog.pub().info("IPCameraDisplay: reconnecting to "+ip+"...");
+        connected = false;        
+    }
+
+    private Thread updaterThread() {
+        return new Thread() {
+            
+            @Override
+            public void run() {
+
+                while(true) {   
+                    if (updater != this)
+                        return;
+
+                    if (ip == null)
+                        break;
+                    connected = true;
+                    try {
+                        URL url = new URL("http://"+ip+"/images/logo.gif");
+                        imageToDisplay = ImageIO.read(url);     
+                        repaint();
+                    }
+                    catch (Exception e) {
+                        status = "Error: "+e.getMessage();
+                        e.printStackTrace();
+                        NeptusLog.pub().warn(e);          
+                        repaint();
+                        connected = false;
+                        status = "reconnecting";
+                    }
+                    finally {
+
+                    }
+
+                    try {
+                        Thread.sleep(millisBetweenTries);
+                    }
+                    catch (Exception e) {
+                        NeptusLog.pub().warn(e);
+                    }
+                }
+                NeptusLog.pub().info("<###>Thread exiting...");
+            }
+        };
+    }
+
+    @Override
+    public void cleanSubPanel() {
+        status = "stopping";
+        ip = null;
+        connected = false;
+        updater.interrupt();
+    }
+
+    protected String previousURL = null;
+    
+    @Override
+    public DefaultProperty[] getProperties() {
+        previousURL = ip;
+        return super.getProperties();
+    }
+    
+    @Override
+    public void propertiesChanged() {
+        if (!ip.equals(previousURL))
+            reconnect();
+    }
+    
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        final AirCamDisplay display = new AirCamDisplay(null);
+        GuiUtils.testFrame(display, "Camera Display");
     }
 
     /* (non-Javadoc)
-     * @see pt.up.fe.dceg.neptus.plugins.SimpleSubPanel#cleanSubPanel()
+     * @see pt.up.fe.dceg.neptus.plugins.SimpleSubPanel#initSubPanel()
      */
     @Override
-    public void cleanSubPanel() {
+    public void initSubPanel() {
         // TODO Auto-generated method stub
-
+        
     }
 
 }
