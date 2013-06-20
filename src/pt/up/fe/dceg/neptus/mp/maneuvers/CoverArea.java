422f9f0f245e4f775cc88d09c0d505e28dbe48cf
Paulo Dias <paulo.sousa.dias@gmail.com>
2013-06-20 12:18:29 +0100

    Added test.

--- src/pt/up/fe/dceg/neptus/mp/maneuvers/CoverArea.java
+++ src/pt/up/fe/dceg/neptus/mp/maneuvers/CoverArea.java
@@ -249,5 +249,13 @@ public class CoverArea extends AbstractImcManeuver<pt.up.fe.dceg.neptus.imc.Cove
         CoverArea area = new CoverArea();
 
         NeptusLog.pub().info("<###> "+area.asXML());
+        
+        CoverArea compc = new CoverArea();
+        String ccmanXML = compc.getManeuverAsDocument("CoverArea").asXML();
+        System.out.println(ccmanXML);
+        CoverArea compc1 = new CoverArea();
+        compc1.loadFromXML(ccmanXML);
+        ccmanXML = compc.getManeuverAsDocument("CoverArea").asXML();
+        System.out.println(ccmanXML);
     }
 }
