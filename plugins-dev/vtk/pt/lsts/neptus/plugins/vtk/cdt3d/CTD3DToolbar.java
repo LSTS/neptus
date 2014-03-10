66163b6c0f4534ed8e5392907279361e44089e78
Hugo Queirós <hugofqueiros@gmail.com>
2014-03-10 15:05:40 +0000

    Plugins:CTD3D: WIP on toolbar.

--- plugins-dev/vtk/pt/lsts/neptus/plugins/vtk/cdt3d/CTD3DToolbar.java
+++ plugins-dev/vtk/pt/lsts/neptus/plugins/vtk/cdt3d/CTD3DToolbar.java
@@ -31,29 +31,74 @@
  */
 package pt.lsts.neptus.plugins.vtk.cdt3d;
 
-import javax.swing.JToggleButton;
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
+import javax.swing.JToggleButton;
+import javax.swing.JToolBar;
+
+import pt.lsts.neptus.i18n.I18n;
 import pt.lsts.neptus.plugins.vtk.CTD3D;
+import pt.lsts.neptus.util.ImageUtils;
 
 /**
  * @author hfq
  *
  */
-public class CTD3DToolbar {
+public class CTD3DToolbar extends JToolBar {
+    private static final long serialVersionUID = 1L;
+
+    private static final short ICON_SIZE = 18;
+
+    private static final ImageIcon ICON_TEMP = ImageUtils.getScaledIcon(
+            "pt/lsts/neptus/plugins/ctd/thermometer.png", ICON_SIZE, ICON_SIZE);
+    private static final ImageIcon ICON_SALINITY = ImageUtils.getScaledIcon(
+            "images/menus/wizard.png", ICON_SIZE, ICON_SIZE);
 
     private JToggleButton tempToggle;
-    private JToggleButton salinitytoggle;
+    private JToggleButton salinityToggle;
 
     private CTD3D ctd3dInit;
 
     /**
      * 
+     * @param ctd3dInit
      */
     public CTD3DToolbar(CTD3D ctd3dInit) {
         this.ctd3dInit = ctd3dInit;
     }
 
     public void createtoolBar() {
+        setOrientation(JToolBar.VERTICAL);
+        setBorder(BorderFactory.createCompoundBorder(BorderFactory.createRaisedBevelBorder(),
+                BorderFactory.createEmptyBorder()));
 
+        tempToggle = new JToggleButton();
+        tempToggle.setToolTipText(I18n.text("See Temperature color map") + ".");
+        tempToggle.setIcon(ICON_TEMP);
+
+        salinityToggle = new JToggleButton();
+        salinityToggle.setToolTipText(I18n.text("See Salinity color map") + ".");
+        salinityToggle.setIcon(ICON_SALINITY);
+
+        ButtonGroup groupToggles = new ButtonGroup();
+        groupToggles.add(tempToggle);
+        groupToggles.add(salinityToggle);
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D graphic2d = (Graphics2D) g;
+        Color color1 = getBackground();
+        Color color2 = Color.GRAY;
+        GradientPaint gradPaint = new GradientPaint(0, 0, color1, getWidth(), getHeight(), color2);
+        graphic2d.setPaint(gradPaint);
+        graphic2d.fillRect(0, 0, getWidth(), getHeight());
     }
 }
