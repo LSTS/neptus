LSTS/neptus

commit 94d4c1b2a14d9f92068e676b630005918bbf9658

Multiple improvements in the SituationAwareness plug-in.

src/pt/lsts/neptus/types/coord/LocationType.java
@@ -703,7 +703,7 @@ public class LocationType implements XmlOutputMethods, Serializable, Comparable<
         }
 
         return latStr + nf6.format(lat) + CoordinateUtil.CHAR_DEGREE + ", " + lonStr
-                + nf6.format(lon) + CoordinateUtil.CHAR_DEGREE + ", " + nf2.format(getHeight());
+                + nf6.format(lon) + CoordinateUtil.CHAR_DEGREE + (getHeight() != 0 ? (", " +  nf2.format(getHeight())) : "");
     }
 
     //    /**

plugins-dev/simulation/pt/lsts/neptus/plugins/sim/SimulatedStatePainter.java
@@ -113,9 +113,6 @@ public class SimulatedStatePainter extends ConsolePanel implements Renderer2DPai
         lastStateMillis = System.currentTimeMillis();
     }
 
-    /* (non-Javadoc)
-     * @see pt.lsts.neptus.plugins.SimpleSubPanel#cleanSubPanel()
-     */
     @Override
     public void cleanSubPanel() {
         // TODO Auto-generated method stub

plugins-dev/state/pt/lsts/neptus/plugins/state/AudibleVehicleState.java
@@ -126,9 +126,14 @@ public class AudibleVehicleState extends ConsolePanel implements IPeriodicUpdate
         return true;
     }
 
+    private String cleanUp(String text) {
+        return text
+                .replaceAll("xtreme", "extreme")
+                .replaceAll("xplore", "explore");
+    }
     public void say(String text) {
-        if (useAudioAlerts)
-            SpeechUtil.readSimpleText(text.replaceAll("xtreme", "extreme"));
+        if (useAudioAlerts) 
+            SpeechUtil.readSimpleText(cleanUp(text));
     }
 
     @Subscribe

plugins-dev/sunfish/pt/lsts/neptus/plugins/sunfish/awareness/AssetPosition.java
@@ -31,7 +31,10 @@
  */
 package pt.lsts.neptus.plugins.sunfish.awareness;
 
+import java.util.LinkedHashMap;
+
 import pt.lsts.neptus.types.coord.LocationType;
+import pt.lsts.neptus.util.DateTimeUtil;
 
 /**
  * @author zp
@@ -46,6 +49,7 @@ public class AssetPosition implements Comparable<AssetPosition> {
     private double accuracy = Double.NaN;
     private String source = "unknown";
     private String type = "Sensor";
+    private LinkedHashMap<String, String> extraInfo = new LinkedHashMap<String, String>();
     
     public AssetPosition(String asset, double latDegrees, double lonDegrees) {
         this.timestamp = System.currentTimeMillis();
@@ -163,5 +167,25 @@ public class AssetPosition implements Comparable<AssetPosition> {
     @Override
     public int hashCode() {
         return (assetName + "_" + (getTimestamp()/1000)).hashCode();
+    }
+    
+    public void putExtra(String key, String value) {
+        extraInfo.put(key, value);
+    }
+    
+    public String getHtml() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("<html>").
+            append("<table><tr><th colspan='2'>"+getAssetName()+"</th></tr>").
+            append("<tr><td>Type:</td><td>"+getType()+"</td></tr>").
+            append("<tr><td>Source:</td><td>"+getSource()+"</td></tr>").
+            append("<tr><td>Age:</td><td>"+DateTimeUtil.milliSecondsToFormatedString(getAge())+"</td></tr>").
+            append("<tr><td>Location:</td><td>"+getLoc()+"</td></tr>");
+        
+        for (String key : extraInfo.keySet())
+            sb.append("<tr><td>"+key+":</td><td>"+extraInfo.get(key)+"</td></tr>");
+        
+        sb.append("</table></html>");
+        return sb.toString();
     }
 }

plugins-dev/sunfish/pt/lsts/neptus/plugins/sunfish/awareness/SituationAwareness.java
@@ -33,6 +33,8 @@ package pt.lsts.neptus.plugins.sunfish.awareness;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -41,16 +43,19 @@ import java.awt.geom.Ellipse2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Vector;
 
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 
 import org.jdesktop.swingx.JXTable;
+import org.mozilla.javascript.edu.emory.mathcs.backport.java.util.Collections;
 import org.reflections.Reflections;
 
 import pt.lsts.neptus.NeptusLog;
@@ -171,6 +176,7 @@ public class SituationAwareness extends ConsoleInteraction implements IConsoleLa
         Thread.sleep(100000);
     }
 
+    private JLabel lbl = new JLabel();
     @Override
     public void paintInteraction(Graphics2D g, StateRenderer2D source) {
         super.paintInteraction(g, source);
@@ -181,7 +187,15 @@ public class SituationAwareness extends ConsoleInteraction implements IConsoleLa
             Point2D pt = source.getScreenPosition(pivot.getLoc());
             g.setColor(Color.white);
             g.draw(new Ellipse2D.Double(pt.getX() - 6, pt.getY() - 6, 12, 12));
-            g.drawString(pivot.getAssetName() + ", age: " + getAge(pivot), 10, source.getHeight() - 50);
+            //g.drawString(pivot.getAssetName() + ", age: " + getAge(pivot), 10, source.getHeight() - 50);
+            lbl.setOpaque(true);
+            lbl.setBackground(new Color(255,255,255,128));
+            lbl.setText(pivot.getHtml());
+            Dimension d = lbl.getPreferredSize();
+            lbl.setSize(d);
+            Graphics copy = g.create();
+            copy.translate(10, 10);
+            lbl.paint(copy);
         }
 
         for (AssetTrack t : assets.values()) {
@@ -248,6 +262,16 @@ public class SituationAwareness extends ConsoleInteraction implements IConsoleLa
                     PluginUtils.editPluginProperties(SituationAwareness.this, true);
                 }
             });
+            popup.add("Randomize colors").addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    java.util.Random rnd = new java.util.Random();
+                    for (AssetTrack track : assets.values()) {
+                        track.setColor(new Color(rnd.nextInt(255), rnd.nextInt(255), rnd.nextInt(255)));
+                    }
+                }
+            });
+            popup.addSeparator();
             popup.add("Decision Support").addActionListener(new ActionListener() {
                 
                 @Override
@@ -262,6 +286,10 @@ public class SituationAwareness extends ConsoleInteraction implements IConsoleLa
                     LinkedHashMap<String, Vector<AssetPosition>> positions = positionsByType();
                     tags.addAll(positions.get("SPOT Tag"));
                     //FIXME add argos tags
+                    if (!assets.containsKey(uuvAsset)) {
+                        GuiUtils.errorMessage(getConsole(), "Decision Support", "UUV asset position is unknown");
+                        return;
+                    }
                     supportTable.setAssets(assets.get(uuvAsset).getLatest(), tags);
                     dialogDecisionSupport.setContentPane(new JScrollPane(new JXTable(supportTable)));
                     dialogDecisionSupport.invalidate();
@@ -317,15 +345,27 @@ public class SituationAwareness extends ConsoleInteraction implements IConsoleLa
 
     @Override
     public void mouseMoved(MouseEvent event, StateRenderer2D source) {
-        for (AssetTrack track : assets.values()) {
-            for (AssetPosition p : track.getTrack(15, 0)) {
-                double dist = event.getPoint().distance(source.getScreenPosition(p.getLoc()));
-                if (dist < 5) {
-                    intercepted = p;
-                    return;
-                }
+        
+        List<AssetPosition> allPositions = new ArrayList<AssetPosition>();
+        
+        for (AssetTrack track : assets.values())
+            allPositions.addAll(track.getTrack(15, 0));
+        
+        Collections.sort(allPositions, new Comparator<AssetPosition>() {
+            @Override
+            public int compare(AssetPosition o1, AssetPosition o2) {
+                return (int)(o1.getAge() - o2.getAge());
+            }
+        });
+        
+        for (AssetPosition p : allPositions) {
+            double dist = event.getPoint().distance(source.getScreenPosition(p.getLoc()));
+            if (dist < 5) {
+                intercepted = p;
+                return;
             }
         }
+
         intercepted = null;
     }
 }

plugins-dev/sunfish/pt/lsts/neptus/plugins/sunfish/awareness/SpotLocationProvider.java
@@ -38,6 +38,7 @@ import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.WordUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -77,7 +78,7 @@ public class SpotLocationProvider implements ILocationProvider {
                 String name = null;
                 double lat = 0, lon = 0;
                 long timestamp = System.currentTimeMillis();
-                
+                String battState = null, msgType = null;
                 for (int j = 0; j < elems.getLength(); j++) {
                     Node nd = elems.item(j); 
                     switch (nd.getNodeName()) {
@@ -93,6 +94,12 @@ public class SpotLocationProvider implements ILocationProvider {
                     case "messengerName":
                         name = nd.getTextContent();
                         break;
+                    case "batteryState":
+                        battState = nd.getTextContent();
+                        break;
+                    case "messageType":
+                        msgType = nd.getTextContent();
+                        break;
                     default:
                         break;
                     }
@@ -100,7 +107,12 @@ public class SpotLocationProvider implements ILocationProvider {
                 if (name != null) {
                     AssetPosition pos = new AssetPosition(name, lat, lon);
                     pos.setTimestamp(timestamp);
+                    pos.setSource("SPOT (Web)");
                     pos.setType("SPOT Tag");
+                    if (battState != null)
+                        pos.putExtra("Battery", WordUtils.capitalize(battState.toLowerCase()));
+                    if (msgType != null)
+                        pos.putExtra("SPOT Mode", WordUtils.capitalize(msgType.toLowerCase()));
                     parent.addAssetPosition(pos);
                 }
             }

src/pt/lsts/neptus/comm/iridium/DesiredAssetPosition.java
@@ -34,12 +34,12 @@ package pt.lsts.neptus.comm.iridium;
 import java.util.Collection;
 import java.util.Vector;
 
-import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
-import pt.lsts.neptus.types.coord.LocationType;
+import pt.lsts.imc.IMCDefinition;
 import pt.lsts.imc.IMCInputStream;
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.IMCOutputStream;
 import pt.lsts.imc.RemoteSensorInfo;
+import pt.lsts.neptus.types.coord.LocationType;
 
 /**
  * @author zp
@@ -95,8 +95,9 @@ public class DesiredAssetPosition extends IridiumMessage {
         RemoteSensorInfo sensorInfo = new RemoteSensorInfo();
         sensorInfo.setLat(getLocation().getLatitudeRads());
         sensorInfo.setLon(getLocation().getLongitudeRads());
+        sensorInfo.setSensorClass("Desired Position");
         sensorInfo.setAlt(0);
-        sensorInfo.setId("DP_"+ImcSystemsHolder.translateImcIdToSystemName(asset_imc_id).replaceAll(":", ""));
+        sensorInfo.setId("DP_"+IMCDefinition.getInstance().getResolver().resolve(asset_imc_id));
         sensorInfo.setSrc(getSource());
         sensorInfo.setDst(getDestination());
         msgs.add(sensorInfo);        

src/pt/lsts/neptus/comm/iridium/TargetAssetPosition.java
@@ -34,12 +34,12 @@ package pt.lsts.neptus.comm.iridium;
 import java.util.Collection;
 import java.util.Vector;
 
-import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
-import pt.lsts.neptus.types.coord.LocationType;
+import pt.lsts.imc.IMCDefinition;
 import pt.lsts.imc.IMCInputStream;
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.IMCOutputStream;
 import pt.lsts.imc.RemoteSensorInfo;
+import pt.lsts.neptus.types.coord.LocationType;
 
 /**
  * @author zp
@@ -95,8 +95,9 @@ public class TargetAssetPosition extends IridiumMessage {
         RemoteSensorInfo sensorInfo = new RemoteSensorInfo();
         sensorInfo.setLat(getLocation().getLatitudeRads());
         sensorInfo.setLon(getLocation().getLongitudeRads());
+        sensorInfo.setSensorClass("Target Position");
         sensorInfo.setAlt(0);
-        sensorInfo.setId("TP_"+ImcSystemsHolder.translateImcIdToSystemName(asset_imc_id).replaceAll(":", ""));
+        sensorInfo.setId("TP_"+IMCDefinition.getInstance().getResolver().resolve(asset_imc_id));
         sensorInfo.setSrc(getSource());
         sensorInfo.setDst(getDestination());
         msgs.add(sensorInfo);        

commit f2e38c9058ad4ba120167ea41a2680485d2a632b

LocationType: Fix a bug introduced in 94d4c1b2 (25-03-2014) where the toString() if height is 0 does not output it. This cause a problem in the related method valueOf(..) that was expecting it.

src/pt/lsts/neptus/types/coord/LocationType.java
@@ -1329,9 +1329,7 @@ public class LocationType implements XmlOutputMethods, Serializable, Comparable<
         LocationType loc = new LocationType();
         loc.setLatitudeStr(parts[0]);
         loc.setLongitudeStr(parts[1]);
-        loc.setHeight(Double.parseDouble(parts[2]));
-        
-
+        loc.setHeight(parts.length < 3 ? 0 : Double.parseDouble(parts[2]));
         return loc;
     }
     