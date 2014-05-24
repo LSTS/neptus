91a4728273c52bd8e78d0b59d421ce27051bec35
Paulo Dias <paulo.sousa.dias@gmail.com>
2014-05-24 16:24:19 +0100

    Joystick/ControllerPanel: Adding a proper cleanup. It was not being called and not executing a proper unregister of PeriodicUpdatesService and console removeMainVehicleListener(..).

--- src/pt/lsts/neptus/controllers/ControllerPanel.java
+++ src/pt/lsts/neptus/controllers/ControllerPanel.java
@@ -214,6 +214,14 @@ public class ControllerPanel extends ConsolePanel implements IPeriodicUpdates {
         refreshInterface();
     }
 
+    @Override
+    public void cleanSubPanel() {
+        // Unregister listeners
+        console.removeMainVehicleListener(this);
+        PeriodicUpdatesService.unregister(this);
+        ImcMsgManager.getManager().removeListener(this);
+    }
+    
     public void buildDialog() {
         removeAll();
         setSize(300, 200);
@@ -468,10 +476,6 @@ public class ControllerPanel extends ConsolePanel implements IPeriodicUpdates {
         buildDialog();
     }
 
-    public void cleanup() {
-        ImcMsgManager.getManager().removeListener(this);
-    }
-
     /**
      * @author jqcorreia
      */
@@ -513,11 +517,6 @@ public class ControllerPanel extends ConsolePanel implements IPeriodicUpdates {
             });
 
         }
-    }
-    
-    @Override
-    public void cleanSubPanel() {
-        
     }
     
     @SuppressWarnings("serial")
