90eed5bd32a7d490e1a8ebed8ccb4c3d2fde8c3a
sergioferreira <a.sergio.s.ferreira@gmail.com>
2014-06-15 06:49:21 +0100

    Initial prototype of speed panel indicating: air speed | ground speed and plan speed must mark vstall threshold must re-work icons

--- plugins-dev/uavs/pt/lsts/neptus/plugins/uavs/panels/SpeedIndicatorPanel.java
+++ plugins-dev/uavs/pt/lsts/neptus/plugins/uavs/panels/SpeedIndicatorPanel.java
@@ -31,22 +31,24 @@
  */
 package pt.lsts.neptus.plugins.uavs.panels;
 
+import java.awt.Color;
 import java.text.DecimalFormat;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
-import com.google.common.eventbus.Subscribe;
-
+import net.miginfocom.swing.MigLayout;
 import pt.lsts.imc.IMCDefinition;
 import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.Maneuver;
-import pt.lsts.imc.PlanControl;
 import pt.lsts.imc.PlanControlState.STATE;
 import pt.lsts.imc.PlanManeuver;
 import pt.lsts.imc.PlanSpecification;
 import pt.lsts.neptus.comm.IMCSendMessageUtils;
 import pt.lsts.neptus.comm.manager.imc.ImcId16;
-import pt.lsts.neptus.comm.manager.imc.ImcMsgManager;
 import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.ConsolePanel;
@@ -58,6 +60,8 @@ import pt.lsts.neptus.plugins.NeptusProperty;
 import pt.lsts.neptus.plugins.PluginDescription;
 import pt.lsts.neptus.plugins.PluginDescription.CATEGORY;
 import pt.lsts.neptus.util.ImageUtils;
+
+import com.google.common.eventbus.Subscribe;
 
 
 /**
@@ -74,18 +78,19 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
     
     private static final long serialVersionUID = 1L;
 
-    @NeptusProperty(name="Minimum Speed", description="Speed below which the vehicle enters VStall (carefull with units)")
-    public double minSpeed = 14.0;
+    @NeptusProperty(name="Minimum Speed", description="Speed below which the vehicle enters VStall (m/s)")
+    public double minSpeed = 12.0;
     
-    @NeptusProperty(name="Maximum Speed", description="Speed above which it's undesirable to fly (carefull with units)")
-    public double maxSpeed = 22.0;
+    @NeptusProperty(name="Maximum Speed", description="Speed above which it's undesirable to fly (m/s)")
+    public double maxSpeed = 25.0;
     
-    @NeptusProperty(name="Units", description="Units of measurement (check for m/s and uncheck for knot)")
-    public boolean isSIUnit = true;
-    
+    //To be used if other speed units are desired
     static public final double MS_TO_KNOTS_CONV = 1.94384449244;
     
-    private final ImageIcon ICON_UP = ImageUtils.getIcon("images/planning/up.png");
+    //Illustrative icons to differentiate speeds
+    private ImageIcon ICON_TSPEED;
+    private ImageIcon ICON_GSPEED;
+    private ImageIcon ICON_ASPEED;
         
     //indicates if the UAV as changed plan and needs to update it's command speed value
     private boolean samePlan = false;
@@ -95,12 +100,29 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
     
     private PlanSpecification planSpec = null;
     
-    private double aSpeed;
-    private double gSpeed;
-    private double cSpeed;
+    //various speeds
+    private double aSpeed = 0.0;
+    private double gSpeed = 0.0;
+    private double tSpeed = 0.0;
+    
+    //sub panels used to better accommodate the information through the use of the layout manager
+    private JPanel titlePanel = null;
+    private JPanel topLabelPanel = null; 
+    private JPanel speedPanel = null;
+    private JPanel speedNumberPanel = null;
+    private JPanel speedGraphPanel = null; 
+    private JPanel bottomLabelPanel = null;
+    
+    private JPanel aSpeedPanel = null;
+    private JPanel gSpeedPanel = null;
+    
+    private JLabel aSpeedLabel = null;
+    private JLabel gSpeedLabel = null;
+    private JLabel tSpeedLabel = null;
+    private JLabel maxSpeedLabel = null;
     
     //display output formatter
-    private DecimalFormat formatter = new DecimalFormat("0.00");
+    private DecimalFormat formatter = new DecimalFormat("0.0");
     
     //listener object which allows the panel to tap into the various IMC messages
     private MultiSystemIMCMessageListener listener;
@@ -120,18 +142,24 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
             @Override
             public void messageArrived(ImcId16 id, IMCMessage msg) {
 
-               if(msg.getAbbrev().equals("EstimatedState")){
-                   gSpeed = Math.sqrt(
-                           msg.getDouble("vx") * msg.getDouble("vx") +
-                           msg.getDouble("vy") * msg.getDouble("vy") +
-                           msg.getDouble("vz") * msg.getDouble("vz"));
+                //gets the direct values for ground and air speed
+                if(msg.getAbbrev().equals("EstimatedState")){
+                    gSpeed = Math.sqrt(
+                            msg.getDouble("vx") * msg.getDouble("vx") +
+                            msg.getDouble("vy") * msg.getDouble("vy") +
+                            msg.getDouble("vz") * msg.getDouble("vz"));
                    
-                   aSpeed = Math.sqrt(
-                           msg.getDouble("u") * msg.getDouble("u") +
-                           msg.getDouble("v") * msg.getDouble("v") +
-                           msg.getDouble("w") * msg.getDouble("w"));
+                    aSpeed = Math.sqrt(
+                            msg.getDouble("u") * msg.getDouble("u") +
+                            msg.getDouble("v") * msg.getDouble("v") +
+                            msg.getDouble("w") * msg.getDouble("w"));
+                    
+                    //speeds updated
+                    speedLabelPositionUpdate();
                }
                else if(msg.getAbbrev().equals("PlanControlState")){
+                   //if the vehicle is currently executing a plan we ask for that plan 
+                   //and then identify what maneuver is being executed                   
                    if(msg.getAsNumber("state").longValue() == STATE.EXECUTING.value()){    
                        
                        if(!msg.getAsString("plan_id").equals(currentPlan))
@@ -144,7 +172,10 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
                            for(PlanManeuver planMan: planSpec.getManeuvers()){
                                if(planMan.getManeuverId().equals(currentManeuver)){
                                    Maneuver man = planMan.getData();
-                                   cSpeed = man.getAsNumber("speed").doubleValue();
+                                   tSpeed = man.getAsNumber("speed").doubleValue();
+                                   
+                                   //plan updated
+                                   tSpeedLabelPositionUpdate();
                                }
                            }
                        }
@@ -164,15 +195,14 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
                    }
                }
                else if(msg.getAbbrev().equals("PlanControl") && !samePlan){
-
+                   
                    if(msg.getMessage("arg").getAbbrev().equals("PlanSpecification")){
                        planSpec = (PlanSpecification) msg.getMessage("arg");
                        samePlan = true;                      
                    }                   
                }
 
-//               System.out.println("gSpeed: "+formatter.format(gSpeed)+" aSpeed: "+formatter.format(aSpeed)+" cSpeed: "+formatter.format(cSpeed));
-               repaint();
+               update();              
             }
         };        
     }
@@ -180,37 +210,126 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
     @Override
     public void initSubPanel() {
         setListener();
-//        setLayers(new LinkedHashMap<String, IUavPainter>());
-//        setVehicleAltitudes(new LinkedHashMap<String, Integer>());
-//        setArgs(new LinkedHashMap<String, Object>());
-
+        
         // sets up the listener to listen to all vehicles
         listener.setSystemToListen(ImcSystemsHolder.lookupSystemByName(getConsole().getMainSystem()).getId());
 
         // which messages are listened to
-        listener.setMessagesToListen("EstimatedState","PlanControlState","PlanControl");
+        listener.setMessagesToListen("EstimatedState","PlanControlState","PlanControl");       
         
- 
+        ICON_TSPEED = ImageUtils.createScaleImageIcon("pt/lsts/neptus/plugins/uavs/icons/target.png",(int)(this.getHeight()*0.2),(int)(this.getHeight()*0.2));
+        ICON_GSPEED = ImageUtils.createScaleImageIcon("pt/lsts/neptus/plugins/uavs/icons/ground.png",(int)(this.getHeight()*0.2),(int)(this.getHeight()*0.2));
+        ICON_ASPEED = ImageUtils.createScaleImageIcon("pt/lsts/neptus/plugins/uavs/icons/air.png",(int)(this.getHeight()*0.2),(int)(this.getHeight()*0.2));
         
-//        // sets up all the layers used by the panel
-//        layers.put("Skybox", new UavCoverLayerPainter("Skybox"));
-//        layers.put("SidePanel", new UavCoverLayerPainter("SidePanel"));
-//        layers.put("AltitudeLabel", new UavLabelPainter("AltitudeLabel"));
-//        layers.put("UavRulerPainter",  new UavRulerPainter("Ruler"));
-//        layers.put("UavMissionElement", new UavMissionElementPainter("Uavs"));     
-                                
-//        // sets up initial colors for the cover panels
-//        args.put("Skybox.Color", new Color[] {Color.blue,Color.gray.brighter()});
-//        args.put("SidePanel.Color",  new Color[] {Color.gray,Color.gray});
-//        args.put("AltitudeLabel.Color",  new Color[] {Color.gray.brighter(),Color.gray.brighter()});
-//       
-//        this.addComponentListener(this);
-//        
-//        updateLabelText();
-//        updatePainterSizes();  
+        titlePanelSetup();
+        topLabelPanelSetup();
+        bottomLabelPanelSetup();
+        speedPanelSetup();
         
+        //panel general layout setup
+        this.setLayout(new MigLayout("gap 0 0, ins 0"));
+        this.add(titlePanel,"w 100%, h 15%!, wrap"); 
+        this.add(topLabelPanel,"w 100%, h 15%!, wrap"); 
+        this.add(speedPanel,"w 100%, h 50%!, wrap"); 
+        this.add(bottomLabelPanel,"w 100%, h 20%!");
     }
 
+    /**
+     * 
+     */
+    private void titlePanelSetup() {
+        titlePanel = new JPanel(new MigLayout("gap 0 0, ins 0"));
+        titlePanel.add(new JLabel(I18n.text("Speed Indicator"),SwingConstants.CENTER), "w 100%, h 100%");
+    }
+    
+    /**
+     * 
+     */
+    private void topLabelPanelSetup() {
+        maxSpeedLabel = new JLabel(formatter.format(maxSpeed),SwingConstants.LEFT);    
+        topLabelPanel = new JPanel(new MigLayout("gap 0 0, ins 0, rtl"));
+        
+            JPanel tempPanel2 = new JPanel(new MigLayout("ins 0"));                    
+            tempPanel2.add(maxSpeedLabel, "w 50%, h 100%");
+            tempPanel2.add(new JLabel("0.0",SwingConstants.RIGHT), "w 50%, h 100%");
+        
+        topLabelPanel.add(tempPanel2, "w 80%, h 100%");
+    }
+    
+    /**
+     * 
+     */
+    private void speedPanelSetup() {
+        speedPanel = new JPanel(new MigLayout("gap 0 0, ins 0"));
+        
+            speedNumberPanel = new JPanel(new MigLayout("ins 0"));
+            aSpeedLabel = new JLabel(ICON_ASPEED);
+            gSpeedLabel = new JLabel(ICON_GSPEED);
+            aSpeedLabel.setHorizontalTextPosition(SwingConstants.LEADING);
+            gSpeedLabel.setHorizontalTextPosition(SwingConstants.LEADING);
+            aSpeedLabel.setHorizontalAlignment(SwingConstants.RIGHT); 
+            gSpeedLabel.setHorizontalAlignment(SwingConstants.RIGHT); 
+            speedNumberPanel.add(aSpeedLabel, "w 100%, h 50%, wrap");
+            speedNumberPanel.add(gSpeedLabel, "w 100%, h 50%");
+            
+            speedGraphPanel = new JPanel(new MigLayout("ins 0, rtl"));
+            speedGraphPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+            aSpeedPanel = new JPanel();
+            aSpeedPanel.setBackground(Color.cyan.darker());
+            gSpeedPanel = new JPanel();
+            gSpeedPanel.setBackground(Color.GREEN.darker());
+            speedLabelPositionUpdate();
+                                    
+        speedPanel.add(speedNumberPanel, "w 20%, h 100%");
+        speedPanel.add(speedGraphPanel, "w 80%, h 100%");
+    }
+    
+    /**
+     * 
+     */
+    private void bottomLabelPanelSetup() {               
+        bottomLabelPanel = new JPanel(new MigLayout("gap 0 0, ins 0, rtl"));      
+        tSpeedLabel = new JLabel(ICON_TSPEED);
+        tSpeedLabel.setHorizontalTextPosition(SwingConstants.LEADING);
+        tSpeedLabel.setHorizontalAlignment(SwingConstants.LEFT);       
+        
+        tSpeedLabelPositionUpdate();
+    }
+    
+    /**
+     * 
+     */
+    private void speedLabelPositionUpdate() {
+        //lazy workaround
+        maxSpeedLabel.setText(formatter.format(maxSpeed));
+        aSpeedLabel.setText(formatter.format(aSpeed));
+        gSpeedLabel.setText(formatter.format(gSpeed));
+        
+        int tPercent = (int)(aSpeed*100/maxSpeed);
+
+        speedGraphPanel.remove(aSpeedPanel);
+        speedGraphPanel.add(aSpeedPanel, "w "+tPercent+"%, h 50%, wrap");
+                
+        int tPercent2 = (int)(gSpeed*100/maxSpeed);
+        
+        speedGraphPanel.remove(gSpeedPanel);
+        speedGraphPanel.add(gSpeedPanel, "w "+tPercent2+"%, h 50%");
+        
+        revalidate();
+    }
+    
+    /**
+     * 
+     */
+    private void tSpeedLabelPositionUpdate() {
+        tSpeedLabel.setText(formatter.format(tSpeed));
+        int iconSizePercent = ICON_TSPEED.getIconWidth()*50/this.getWidth();
+        int tPercent = (int)(tSpeed*80/maxSpeed) + tSpeedLabel.getMinimumSize().width*100/this.getWidth() - iconSizePercent; 
+                
+        bottomLabelPanel.remove(tSpeedLabel);
+        bottomLabelPanel.add(tSpeedLabel, "w "+tPercent+"%, h 100%");
+    }
+    
     @Subscribe
     public void mainVehicleChangeNotification(ConsoleEventMainSystemChange ev) {
         listener.setSystemToListen(ImcSystemsHolder.lookupSystemByName(getConsole().getMainSystem()).getId());
@@ -219,5 +338,11 @@ public class SpeedIndicatorPanel extends ConsolePanel implements MainVehicleChan
     @Override
     public void cleanSubPanel() {
         listener.clean();
+    }
+    
+    private void update(){
+//        speedLabelPositionUpdate();
+//        tSpeedLabelPositionUpdate();
+        repaint();
     }
 }
