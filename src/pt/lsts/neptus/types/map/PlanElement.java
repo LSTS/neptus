8b0444fbe7db111088f9ca4379db2d65d8352236
Jose Braga <eejbraga@gmail.com>
2014-07-30 20:19:11 +0100

    PlanElement: using LocationType to calculate distance.

--- src/pt/lsts/neptus/types/map/PlanElement.java
+++ src/pt/lsts/neptus/types/map/PlanElement.java
@@ -560,7 +560,10 @@ public class PlanElement extends AbstractElement implements Renderer2DPainter, P
                             g.translate(0, -dist / 2);
                             g.rotate(Math.PI / 2);
 
-                            txt += GuiUtils.getNeptusDecimalFormat(0).format(dist / renderer.getZoom()) + " m";
+                            LocationType cPos = renderer.getRealWorldLocation(loc);
+                            LocationType pPos = renderer.getRealWorldLocation(previousPoint);
+
+                            txt += GuiUtils.getNeptusDecimalFormat(0).format(cPos.getDistanceInMeters(pPos)) + " m";
 
                             if (Math.abs(angle) > Math.PI / 2) {
                                 g.rotate(Math.PI);
