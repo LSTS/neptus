653a762ff0618cf59e0e6f3326ddb5f501d8d428
zepinto <zepinto@gmail.com>
2014-08-02 12:25:10 +0100

    Removed unneded imports.

--- src/pt/lsts/neptus/mp/maneuvers/CompassCalibration.java
+++ src/pt/lsts/neptus/mp/maneuvers/CompassCalibration.java
@@ -32,9 +32,7 @@
 package pt.lsts.neptus.mp.maneuvers;
 
 import java.awt.Color;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Ellipse2D;
 import java.util.Arrays;
 import java.util.Vector;
