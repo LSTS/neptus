81fc5242aadb19a76ca38953ed61929012f411ee
Manuel R <manuelribeiro@gmail.com>
2015-01-08 13:21:53 +0000

    MRA/SDF: Code format.

--- src/pt/lsts/neptus/mra/importers/sdf/SdfSidescanParser.java
+++ src/pt/lsts/neptus/mra/importers/sdf/SdfSidescanParser.java
@@ -77,17 +77,13 @@ public class SdfSidescanParser implements SidescanParser {
         if(ping == null) return list;
 
         while(ping.getTimestamp() < timestamp2) {
-
             SdfData sboardPboard = ping; // one ping contains both Sboard and Portboard samples
-
             int nSamples = sboardPboard != null ? sboardPboard.getNumSamples() : 0;
             double fData[] = new double[nSamples*2]; // x2 (portboard + sboard in the same ping)  
-
             double avgSboard = 0, avgPboard = 0;
 
             for (int i = 0; i < nSamples; i++) {
                 double r = ping.getPortData()[i];
-
                 avgPboard += r;
             }
 
@@ -103,8 +99,8 @@ public class SdfSidescanParser implements SidescanParser {
             for (int i = 0; i < nSamples; i++) {
                 double r =  i / (double) nSamples;
                 double gain = Math.abs(30.0 * Math.log(r));
-                
                 double pb = sboardPboard.getPortData()[i] * Math.pow(10, gain / config.getTvgGain());
+                
                 fData[nSamples-i-1] = pb / avgPboard;
             }
             
@@ -112,8 +108,8 @@ public class SdfSidescanParser implements SidescanParser {
             for (int i = 0; i < nSamples; i++) {
                 double r = 1 - (i / (double) nSamples);
                 double gain = Math.abs(30.0 * Math.log(r));
-                
                 double sb = sboardPboard.getStbdData()[i] * Math.pow(10, gain / config.getTvgGain());
+                
                 fData[i+nSamples] = sb / avgSboard;
             }
             
@@ -123,7 +119,6 @@ public class SdfSidescanParser implements SidescanParser {
 
             // or pose.setRoll(Math.toRadians(sboardPboard.getHeader().getRoll()));
             pose.setRoll(Math.toRadians(sboardPboard.getHeader().getAuxRoll()));
-
             pose.setYaw(Math.toRadians(sboardPboard.getHeader().getShipHeading()));           
             pose.setAltitude(sboardPboard.getHeader().getAltitude() ); // altitude in meters
             pose.setU(sboardPboard.getHeader().getSpeedFish() / 100.0); // Convert cm/s to m/s
