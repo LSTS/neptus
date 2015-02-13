61d2f7438e63728e57e516d7d05a824ef7a4acd7
Joao Fortuna <fortuna.araujo@gmail.com>
2015-02-13 19:44:00 +0100

    DropMapLayer: Applied suggestions from reviewer. Can now trigger manual release. Moved out of uavs package.

--- plugins-dev/drop/pt/lsts/neptus/plugins/drop/DropMapLayer.java
+++ plugins-dev/drop/pt/lsts/neptus/plugins/drop/DropMapLayer.java
@@ -1,8 +1,9 @@
 /*
- * Copyright (c) 2004-2015 Universidade do Porto - Faculdade de Engenharia
- * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * Copyright (c) 2004-2015 Norwegian University of Science and Technology (NTNU)
+ * Centre for Autonomous Marine Operations and Systems (AMOS)
+ * Department of Engineering Cybernetics (ITK)
  * All rights reserved.
- * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ * O.S. Bragstads plass 2D, 7034 Trondheim, Norway
  *
  * This file is part of Neptus, Command and Control Framework.
  *
@@ -29,7 +30,7 @@
  * Author: João Fortuna
  * Feb 13, 2015
  */
-package pt.lsts.neptus.plugins.uavs;
+package pt.lsts.neptus.plugins.drop;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -40,24 +41,26 @@ import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Point2D;
 
+import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 
-import pt.lsts.imc.EstimatedState;
 import pt.lsts.imc.PowerChannelControl;
 import pt.lsts.imc.PowerChannelControl.OP;
+import pt.lsts.imc.PowerChannelState;
+import pt.lsts.imc.PowerChannelState.STATE;
 import pt.lsts.imc.Target;
 import pt.lsts.imc.Target.Z_UNITS;
-import pt.lsts.neptus.comm.manager.imc.EntitiesResolver;
+import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.plugins.MainVehicleChangeListener;
 import pt.lsts.neptus.gui.PropertiesEditor;
+import pt.lsts.neptus.i18n.I18n;
 import pt.lsts.neptus.plugins.NeptusProperty;
 import pt.lsts.neptus.plugins.PluginDescription;
 import pt.lsts.neptus.plugins.SimpleRendererInteraction;
 import pt.lsts.neptus.renderer2d.Renderer2DPainter;
 import pt.lsts.neptus.renderer2d.StateRenderer2D;
 import pt.lsts.neptus.types.coord.LocationType;
-import pt.lsts.util.WGS84Utilities;
 
 import com.google.common.eventbus.Subscribe;
 
@@ -65,7 +68,7 @@ import com.google.common.eventbus.Subscribe;
  * @author jfortuna
  *
  */
-@PluginDescription(name = "DropMapLayer", icon = "pt/lsts/neptus/plugins/uavs/drop.png")
+@PluginDescription(name = "DropMapLayer", icon = "pt/lsts/neptus/plugins/drop/drop.png")
 public class DropMapLayer extends SimpleRendererInteraction implements Renderer2DPainter, MainVehicleChangeListener {
 
     @NeptusProperty(name = "Target height [m]", description = "Height of location to hit.")
@@ -74,14 +77,12 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
     @NeptusProperty(name = "Drop radius [m]", description = "Circle inside which the drop should hit.")
     public int dropRadius = 20;
 
-    @NeptusProperty(name = "Entity Name", description = "Vehicle Drop control entity name")
-    public String dropEntity = "Drop on Target";
+    @NeptusProperty(name = "Drop PowerChannel Name", description = "Vehicle Drop control power channel name")
+    public String dropPowerChannel = "drop";
 
     private static final long serialVersionUID = 1L;
-
-    protected boolean dropped = false, targetSet = false;
-
-    private LocationType targetPos, dropPos;
+    private LocationType targetPos = null, dropPos = null;
+    private boolean manualDrop = false;
 
     /**
      * @param console
@@ -95,6 +96,9 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
         super.setActive(mode, source);
     }
 
+    /**
+     * On right-click, show popup menu on the map with plug-in options
+     */
     @Override
     public void mouseClicked(MouseEvent event, StateRenderer2D source) {
 
@@ -102,16 +106,17 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
             JPopupMenu popup = new JPopupMenu();
             final LocationType loc = source.getRealWorldLocation(event.getPoint());
 
-            if (targetSet)
-                addStartDropMenu(popup);
+            addStartDropMenu(popup);
             addSetTargetMenu(popup, loc);
             addSettingMenu(popup);
+            popup.addSeparator();
+            addDropNowMenu(popup);
             popup.show(source, event.getPoint().x, event.getPoint().y);
         }
     }
 
     private void addSettingMenu(JPopupMenu popup) {
-        popup.add("Settings").addActionListener(new ActionListener() {
+        popup.add(I18n.text("Settings")).addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -121,7 +126,8 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
     }
 
     private void addStartDropMenu(JPopupMenu popup) {
-        popup.add("Start drop plan").addActionListener(new ActionListener() {
+        JMenuItem item = popup.add(I18n.text("Start drop plan"));
+        item.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -131,37 +137,59 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
                 target.setZ(dropHeight);
                 target.setZUnits(Z_UNITS.HEIGHT);
                 target.setLabel("neptus");
-
                 send(target);
-
-                dropped = false;
+                dropPos = null;
             }
         });
+        item.setEnabled(targetPos != null);
     }
 
     private void addSetTargetMenu(JPopupMenu popup, final LocationType loc) {
-        popup.add("Set target here").addActionListener(new ActionListener() {
+        popup.add(I18n.text("Set target here")).addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
                 loc.convertToAbsoluteLatLonDepth();
                 targetPos = loc;
-                dropped = false;
-                targetSet = true;
+                dropPos = null;
+            }
+        });
+    }
+
+    private void addDropNowMenu(JPopupMenu popup) {
+        popup.add(I18n.text("Drop now!")).addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                PowerChannelControl pcc = new PowerChannelControl();
+                pcc.setName(dropPowerChannel);
+                pcc.setOp(OP.TURN_ON);
+                send(pcc);
+                manualDrop = true;
             }
         });
     }
 
     @Override
+    /**
+     * Always returns true
+     */
     public boolean isExclusive() {
         return true;
     }
 
+    /**
+     * Paints filled circles on the current target and drop positions.
+     */
     @Override
     public void paint(Graphics2D g, StateRenderer2D renderer) {
+        // If the target position has not been set, there is nothing to paint
+        if (targetPos == null)
+            return;
+
         Point2D pt = renderer.getScreenPosition(targetPos);
         g.translate(pt.getX(), pt.getY());
-        if (!dropped)
+        if (dropPos == null)
             g.setColor(Color.orange);
         else
             g.setColor(Color.green);
@@ -173,7 +201,7 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
         double radius = Math.abs(dropRadius * renderer.getZoom());
         g.draw(new Ellipse2D.Double(-radius, -radius, radius * 2, radius * 2));
 
-        if (dropped) {
+        if (dropPos != null) {
             Point2D pt2 = renderer.getScreenPosition(dropPos);
             g.setColor(Color.red);
             g.fill(new Ellipse2D.Double(pt2.getX() - pt.getX() - 5, pt2.getY() - pt.getY() - 5, 10, 10));
@@ -190,28 +218,31 @@ public class DropMapLayer extends SimpleRendererInteraction implements Renderer2
         // TODO Auto-generated method stub
     }
 
+    /**
+     * When a {@link PowerChannelState} with state ON is received, sets the drop position to current location. If the
+     * release was manually requested, a TURN_OFF command is issued to the drop mechanism.
+     *
+     * @param msg Message to check for the current drop state. Will become "dropped" if <code>msg.state == ON</code> .
+     */
     @Subscribe
-    public void on(PowerChannelControl msg) {
+    public void on(PowerChannelState msg) {
         if (!msg.getSourceName().equals(getConsole().getMainSystem()))
             return;
 
-        if (EntitiesResolver.resolveId(getConsole().getMainSystem(), dropEntity) != msg.getSrcEnt())
+        // check if the power channel matches the dropped power channel
+        if (!dropPowerChannel.isEmpty() && !dropPowerChannel.equals(msg.getName()))
             return;
 
-        if (msg.getOp() == OP.TURN_ON)
-            dropped = true;
-    }
+        if (msg.getState() == STATE.ON) {
+            dropPos = ImcSystemsHolder.getSystemWithName(getMainVehicleId()).getLocation();
 
-    @Subscribe
-    public void on(EstimatedState msg) {
-        if (!msg.getSourceName().equals(getConsole().getMainSystem()))
-            return;
-
-        if (dropped)
-            return;
-
-        double pos[] = WGS84Utilities.toLatLonDepth(msg);
-
-        dropPos = new LocationType(pos[0], pos[1]);
+            if (manualDrop) {
+                PowerChannelControl pcc = new PowerChannelControl();
+                pcc.setName(dropPowerChannel);
+                pcc.setOp(OP.TURN_OFF);
+                send(pcc);
+                manualDrop = false;
+            }
+        }
     }
 }
