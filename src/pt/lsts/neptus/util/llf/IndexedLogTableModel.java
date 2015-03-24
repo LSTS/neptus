4ba4cf2f0784f2c01652cd98108f9fb18dbfc7f4
José Pinto <zepinto@gmail.com>
2015-03-24 10:48:12 +0000

    LogTableModel: Always show all messages in the log independently of them being after the last message timestamp. Fixes #3128.

--- src/pt/lsts/neptus/util/llf/IndexedLogTableModel.java
+++ src/pt/lsts/neptus/util/llf/IndexedLogTableModel.java
@@ -108,8 +108,7 @@ public class IndexedLogTableModel extends AbstractTableModel {
     }
 
     public IndexedLogTableModel(IMraLogGroup source, String msgName) {
-        this(source, msgName, -1l, (long) (source.getLsfIndex()
-                .getEndTime() * 1000));
+        this(source, msgName, -1l, Long.MAX_VALUE);
     }
 
     public IndexedLogTableModel(IMraLogGroup source, String msgName, long initTime, long finalTime) {
@@ -140,7 +139,7 @@ public class IndexedLogTableModel extends AbstractTableModel {
         msgNames.add("dst");
         msgNames.add("dst_ent");
         msgNames.addAll(imcMsgType.getFieldNames());
-        
+
         // load the "row <-> msg index" table
         loadIndexes(initTime/1000.0, finalTime/1000.0);
     }
