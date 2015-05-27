97ef2e8a3d4f015530d61715ad48015dae204c25
Marcus Frølich <marcusf@stud.ntnu.no>
2015-05-27 10:35:20 +0200

    plugins/land: change default dist_behind.

--- plugins-dev/land/pt/lsts/neptus/plugins/land/LandMapLayer.java
+++ plugins-dev/land/pt/lsts/neptus/plugins/land/LandMapLayer.java
@@ -109,7 +109,7 @@ public class LandMapLayer extends SimpleRendererInteraction implements Renderer2
     public double dist_infront = -100;
 
     @NeptusProperty(name = "Distance behind [m]", description = "Distance from net to aimingpoint (WP) after net.", category = "Advanced")
-    public double dist_behind = 150;
+    public double dist_behind = 100;
 
     @NeptusProperty(name = "Ignore evasive [bool]", description = "If true: Force landing despite error demanding evasive.", category = "Advanced")
     public boolean ignore_evasive = false;
