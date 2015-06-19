a3d0140cf20909259ca67f6a32a270dd5ac82b6c
Tiago Rodrigues <tiago.carvalho.rodrigues@gmail.com>
2015-06-19 18:16:48 +0100

    neptus/mra/exporters: Now two files are created, one with full beams and other with centerbeam to meet raia requirements.

--- src/pt/lsts/neptus/mra/exporters/CSVBathymetryExporter.java
+++ src/pt/lsts/neptus/mra/exporters/CSVBathymetryExporter.java
@@ -73,9 +73,13 @@ public class CSVBathymetryExporter implements MRAExporter {
 
     private DeltaTParser deltaParser = null;
 
+    // All beams csv
+    private String processResultOutputFileNameAllBeams;
+    private Writer processResultOutputWriterAllBeams;
     
-    private String processResultOutputFileName;
-    private Writer processResultOutputWriter;
+    // Center beam csv
+    private String processResultOutputFileNameCenterBeam;
+    private Writer processResultOutputWriterCenterBeam;
 
     public CSVBathymetryExporter(IMraLogGroup log) {
         this.log = log;
@@ -102,11 +106,27 @@ public class CSVBathymetryExporter implements MRAExporter {
         // To include plan name and timestamp in the csv file name
         String parentFolderAbsolutePath = source.getFile("Data.lsf").getParent();
         String parentFolder = parentFolderAbsolutePath.substring(parentFolderAbsolutePath.lastIndexOf('/') + 1);
-        processResultOutputFileName = folder.getAbsolutePath() + "/"+parentFolder+"-bathymetry-process.csv";
-        boolean fileChecker = initResultOutputFile();
+        
+        // Create all beams csv file and file descriptor
+        processResultOutputFileNameAllBeams = folder.getAbsolutePath() + "/"+parentFolder+"-bathymetry-process-all-beams.csv";
+        boolean fileChecker = initResultOutputFile(processResultOutputFileNameAllBeams, processResultOutputWriterAllBeams);
         if (!fileChecker) {
-            pmonitor.setNote("Shit Sherlock");
-            return "Shit Sherlock";
+            pmonitor.setNote("File already exists!");
+            return "File already exists!";
+        }
+        if (fileChecker) {
+            processResultOutputWriterAllBeams = initializeWriter(processResultOutputFileNameAllBeams);
+        }
+        
+        // Create center beam csv file and file descriptor
+        processResultOutputFileNameCenterBeam = folder.getAbsolutePath() + "/"+parentFolder+"-bathymetry-process-center-beam.csv";
+        fileChecker = initResultOutputFile(processResultOutputFileNameCenterBeam, processResultOutputWriterCenterBeam);
+        if (!fileChecker) {
+            pmonitor.setNote("File already exists!");
+            return "File already exists!";
+        }
+        if (fileChecker) {
+            processResultOutputWriterCenterBeam = initializeWriter(processResultOutputFileNameCenterBeam);
         }
         
         log = source;
@@ -117,7 +137,80 @@ public class CSVBathymetryExporter implements MRAExporter {
         deltaParser = new DeltaTParser(source);
         
         BathymetrySwath nextSwath = null; 
-        recordMsg("%Time UTC,Latitude Degrees,Longitude Degrees,Roll Radians,Pitch Radians,Yaw Radians, Number of data elem, (X-Offset Y-Offset Heights Meters)* \n");
+        
+        /* Wirte all beams to csv */
+        // Center beam csv first line
+        recordMsg("%Time UTC,Latitude Degrees,Longitude Degrees,Roll Radians,Pitch Radians,Yaw Radians, Number of data elem, (X-Offset Y-Offset Height - meters) \n", processResultOutputWriterCenterBeam);
+        // All beams csv first line        
+        recordMsg("%Time UTC,Latitude Degrees,Longitude Degrees,Roll Radians,Pitch Radians,Yaw Radians, Number of data elem, (X-Offset Y-Offset Heights Meters)* \n", processResultOutputWriterAllBeams);
+        long previousTimeStamp = 0;
+        try {
+            nextSwath = deltaParser.nextSwath();
+            while (nextSwath != null) {
+            
+                long timeInSeconds = (long) (nextSwath.getTimestamp() / 1000);
+                // Only one beam array for second
+                if (previousTimeStamp == 0 || previousTimeStamp != timeInSeconds) {
+
+                    // Timestamp
+                    recordMsg(timeInSeconds * 1000+",", processResultOutputWriterAllBeams);
+                    // Position in degrees
+                    recordMsg(nextSwath.getPose().getPosition().getLatitudeDegs()+",", processResultOutputWriterAllBeams);
+                    recordMsg(nextSwath.getPose().getPosition().getLongitudeDegs()+",", processResultOutputWriterAllBeams);
+                    // Attitude in radians
+                    recordMsg(nextSwath.getPose().getRoll()+",", processResultOutputWriterAllBeams);
+                    recordMsg(nextSwath.getPose().getPitch()+",", processResultOutputWriterAllBeams);
+                    recordMsg(nextSwath.getPose().getYaw()+",", processResultOutputWriterAllBeams);
+                    
+                    
+                    // Timestamp
+                    recordMsg(timeInSeconds * 1000+",", processResultOutputWriterCenterBeam);
+                    // Position in degrees
+                    recordMsg(nextSwath.getPose().getPosition().getLatitudeDegs()+",", processResultOutputWriterCenterBeam);
+                    recordMsg(nextSwath.getPose().getPosition().getLongitudeDegs()+",", processResultOutputWriterCenterBeam);
+                    // Attitude in radians
+                    recordMsg(nextSwath.getPose().getRoll()+",", processResultOutputWriterCenterBeam);
+                    recordMsg(nextSwath.getPose().getPitch()+",", processResultOutputWriterCenterBeam);
+                    recordMsg(nextSwath.getPose().getYaw()+",", processResultOutputWriterCenterBeam);
+                    
+                    // Number of beams
+                    recordMsg("1,", processResultOutputWriterCenterBeam);
+                    recordMsg(nextSwath.getData().length+",", processResultOutputWriterAllBeams);
+                    System.out.println("Size: "+nextSwath.getData().length);
+                    for (int i = 0; i < nextSwath.getData().length; i++) {
+                        if (nextSwath.getData()[i] != null) {
+                            // Beam x offset
+                            recordMsg(nextSwath.getData()[i].north+" ", processResultOutputWriterAllBeams);
+                            // Beam y offset
+                            recordMsg(nextSwath.getData()[i].east+" ", processResultOutputWriterAllBeams);
+                            // Beam Height
+                            recordMsg(nextSwath.getData()[i].depth+",", processResultOutputWriterAllBeams);
+                        }
+                        else
+                            recordMsg("NaN NaN NaN,", processResultOutputWriterAllBeams);
+                        
+                        if (nextSwath.getData()[i] != null && i == 240) {
+                            // Beam x offset
+                            recordMsg(nextSwath.getData()[i].north+" ", processResultOutputWriterCenterBeam);
+                            // Beam y offset
+                            recordMsg(nextSwath.getData()[i].east+" ", processResultOutputWriterCenterBeam);
+                            // Beam Height
+                            recordMsg(nextSwath.getData()[i].depth+",", processResultOutputWriterCenterBeam);
+                        }
+                        else if (nextSwath.getData()[i] == null && i == 240)
+                            recordMsg("NaN NaN NaN,", processResultOutputWriterCenterBeam);
+                    } 
+                    recordMsgln("", processResultOutputWriterCenterBeam);
+                    
+                    recordMsgln("", processResultOutputWriterAllBeams);
+                    
+                    previousTimeStamp = timeInSeconds;
+                }                          
+
+                nextSwath = deltaParser.nextSwath();
+        
+        /*BathymetrySwath nextSwath = null; 
+        recordMsg("%Time UTC,Latitude Degrees,Longitude Degrees,Roll Radians,Pitch Radians,Yaw Radians, Number of data elem, (X-Offset Y-Offset Height - meters) \n");
         long previousTimeStamp = 0;
         try {
             nextSwath = deltaParser.nextSwath();
@@ -137,9 +230,9 @@ public class CSVBathymetryExporter implements MRAExporter {
                     recordMsg(nextSwath.getPose().getPitch()+",");
                     recordMsg(nextSwath.getPose().getYaw()+",");
                     // Number of beams
-                    recordMsg(nextSwath.getData().length+",");
+                    recordMsg("1,");
                     for (int i = 0; i < nextSwath.getData().length; i++) {
-                        if (nextSwath.getData()[i] != null) {
+                        if (nextSwath.getData()[i] != null && i == 240) {
                             // Beam x offset
                             recordMsg(nextSwath.getData()[i].north+" ");
                             // Beam y offset
@@ -147,7 +240,7 @@ public class CSVBathymetryExporter implements MRAExporter {
                             // Beam Height
                             recordMsg(nextSwath.getData()[i].depth+",");
                         }
-                        else
+                        else if (nextSwath.getData()[i] == null && i == 240)
                             recordMsg("NaN NaN NaN,");
                     } 
                     recordMsgln("");
@@ -155,7 +248,7 @@ public class CSVBathymetryExporter implements MRAExporter {
                     previousTimeStamp = timeInSeconds;
                 }                          
 
-                nextSwath = deltaParser.nextSwath();
+                nextSwath = deltaParser.nextSwath();*/
                 
                 if (pmonitor != null && pmonitor.isCanceled())
                     return "Export interrupted!";
@@ -173,11 +266,11 @@ public class CSVBathymetryExporter implements MRAExporter {
 
     }
 
-    private synchronized boolean initResultOutputFile() {
+    private synchronized boolean initResultOutputFile(String processResultOutputFileName, Writer processResultOutputWriter) {
         if (processResultOutputWriter != null || new File(processResultOutputFileName).exists()) {
             return false;
         }
-        try {
+        /*try {
             try {
                 processResultOutputWriter = new OutputStreamWriter(new FileOutputStream(processResultOutputFileName), "UTF-8");
             }
@@ -189,33 +282,48 @@ public class CSVBathymetryExporter implements MRAExporter {
         }
         catch (Exception e) {
             e.printStackTrace();
-        }
+        }*/
         return true;
+    }
+    
+    private Writer initializeWriter(String processResultOutputFileName) {
+        try {
+            return new OutputStreamWriter(new FileOutputStream(processResultOutputFileName), "UTF-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return null;
     }
 
     private void cleanupResultOutputFile() {
-        if (processResultOutputWriter != null) {
+        if (processResultOutputWriterAllBeams != null) {
             try {
-                processResultOutputWriter.flush();
+                processResultOutputWriterAllBeams.flush();
             }
             catch (IOException e) {
                 e.printStackTrace();
             }
             try {
-                processResultOutputWriter.close();
+                processResultOutputWriterAllBeams.close();
             }
             catch (IOException e) {
                 e.printStackTrace();
             }
-            processResultOutputWriter = null;
+            processResultOutputWriterAllBeams = null;
         }
     }
     
-    private void recordMsgln(String string) {
-        recordMsg(string + "\r\n");
+    private void recordMsgln(String string, Writer processResultOutputWriter) {
+        recordMsg(string + "\r\n", processResultOutputWriter);
     }
 
-    private void recordMsg(String string) {
+    private void recordMsg(String string, Writer processResultOutputWriter) {
         if (processResultOutputWriter != null) {
             try {
                 processResultOutputWriter.write(string);
