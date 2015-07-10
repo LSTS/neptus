1cf0950748dcba47831b789ab3a034141fe0e55e
tsmarques <samarques.tiago@gmail.com>
2015-07-10 14:25:33 +0100

    hyperspectral/HyperspectralReplay: Close wavelength selection JFrame when MRA is closed.

--- plugins-dev/hyperspectral/pt/lsts/neptus/hyperspectral/HyperspectralReplay.java
+++ plugins-dev/hyperspectral/pt/lsts/neptus/hyperspectral/HyperspectralReplay.java
@@ -37,6 +37,7 @@ import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
 import java.awt.image.AffineTransformOp;
@@ -147,7 +148,7 @@ public class HyperspectralReplay extends JFrame implements LogReplayLayer {
             g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
             /* draw data along the vehicle's path */
-            for(int i = 0; i < dataList.size(); i++) {
+            for(int i = 0; i < 5000; i++) {
                 HyperspectralData frame = dataList.get(i);
                 Point2D dataPosition = renderer.getScreenPosition(frame.dataLocation);
 
@@ -241,7 +242,7 @@ public class HyperspectralReplay extends JFrame implements LogReplayLayer {
 
     @Override
     public void cleanup() {
-
+        this.dispatchEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSING));
     }
 
     private class HyperspectralData {
