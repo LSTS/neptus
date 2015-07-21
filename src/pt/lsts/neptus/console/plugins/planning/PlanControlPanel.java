4dd98e136bf95b94408941f38240dc615e421d33
Jose Pinto <zepinto@gmail.com>
2015-07-21 14:40:52 +0000

    Ability to ignore errors for the commanded plan (Ctrl-Click).

--- src/pt/lsts/neptus/console/plugins/planning/PlanControlPanel.java
+++ src/pt/lsts/neptus/console/plugins/planning/PlanControlPanel.java
@@ -417,7 +417,10 @@ LockableSubPanel, IPeriodicUpdates, NeptusMessageListener {
                         NeptusLog.action().info(action);
 
                         sendStartButton.setEnabled(false);
-                        sendStartPlan(getSystemsToSendTo(SystemsSelectionAction.getClearSelectionOption(ev)));
+                        boolean ignoreErrors = ((ev.getModifiers() & ActionEvent.CTRL_MASK) == ActionEvent.CTRL_MASK);
+                        if (ignoreErrors)
+                            System.out.println("Control pressed, ignoring errors");
+                        sendStartPlan(ignoreErrors, getSystemsToSendTo(SystemsSelectionAction.getClearSelectionOption(ev)));
 
                         return null;
                     }
@@ -447,7 +450,7 @@ LockableSubPanel, IPeriodicUpdates, NeptusMessageListener {
                         NeptusLog.action().info(action);
 
                         sendStopButton.setEnabled(false);
-                        sendStopPlan(getSystemsToSendTo(SystemsSelectionAction.getClearSelectionOption(ev)));
+                        sendStopPlan(false, getSystemsToSendTo(SystemsSelectionAction.getClearSelectionOption(ev)));
 
                         return null;
                     }
@@ -935,15 +938,15 @@ LockableSubPanel, IPeriodicUpdates, NeptusMessageListener {
         return ret;
     }
 
-    private boolean sendStartPlan(String... systems) {
-        return sendStartStop(PlanControl.OP.START, systems);
+    private boolean sendStartPlan(boolean ignoreErrors, String... systems) {
+        return sendStartStop(ignoreErrors, PlanControl.OP.START, systems);
     }
 
-    private boolean sendStopPlan(String... systems) {
-        return sendStartStop(PlanControl.OP.STOP, systems);
+    private boolean sendStopPlan(boolean ignoreErrors, String... systems) {
+        return sendStartStop(ignoreErrors, PlanControl.OP.STOP, systems);
     }
 
-    private boolean sendStartStop(PlanControl.OP cmd, String... systems) {
+    private boolean sendStartStop(boolean ignoreErrors, PlanControl.OP cmd, String... systems) {
         if (!checkConditionToRun(this, cmd == PlanControl.OP.START ? false : true, cmd == PlanControl.OP.STOP ? false
                 : true))
             return false;
@@ -954,6 +957,9 @@ LockableSubPanel, IPeriodicUpdates, NeptusMessageListener {
         PlanControl pc = new PlanControl();
         pc.setType(PlanControl.TYPE.REQUEST);
         pc.setRequestId(reqId);
+        
+        
+        
         String cmdStrMsg = "";
         try {
             switch (cmd) {
@@ -987,6 +993,10 @@ LockableSubPanel, IPeriodicUpdates, NeptusMessageListener {
         }
         pc.setOp(cmd);
 
+        if(ignoreErrors) {
+            pc.setFlags(pc.getFlags() | PlanControl.FLG_IGNORE_ERRORS);
+            post(Notification.warning(I18n.text("Send Plan"), "Ignoring any errors during execution of this plan"));
+        }
         boolean dontSendByAcoustics = DONT_USE_ACOUSTICS;
         if (cmd == PlanControl.OP.START) {
             String planId = pc.getPlanId();
