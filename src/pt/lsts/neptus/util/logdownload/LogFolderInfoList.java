c18df4da15dfed6fc3b552fc77634f5c944a3c01
Paulo Dias <paulo.sousa.dias@gmail.com>
2016-01-18 16:59:52 +0000

    Revert "util/logdownload/LogFolderInfoList: Little cleanup."
    This reverts commit e15d72d0ee6895dd7b4f35e4aede192602b085e8.
    To avoid unnecessary conflicts.

--- src/pt/lsts/neptus/util/logdownload/LogFolderInfoList.java
+++ src/pt/lsts/neptus/util/logdownload/LogFolderInfoList.java
@@ -98,7 +98,9 @@ JXList provides api to access items of the underlying model in view coordinates 
 	 */
 	public void initialize() {
 		setModel(myModel);
+		//setVisibleRowCount(25);
 		setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+		//setDragEnabled(true);
 		setCellRenderer(new ListCellRenderer<LogFolderInfo>() {
 			
 			public Component getListCellRendererComponent(JList<? extends LogFolderInfo> list, 
@@ -184,6 +186,7 @@ JXList provides api to access items of the underlying model in view coordinates 
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+//				NeptusLog.pub().info("<###>"+((LogFolder)getSelectedValue()).logFiles.size());
 				if (e.getButton() == MouseEvent.BUTTON1
 						&& e.getModifiers() == MouseEvent.CTRL_DOWN_MASK
 						&& e.getClickCount() == 1) {
@@ -191,10 +194,28 @@ JXList provides api to access items of the underlying model in view coordinates 
 				}
 				
 				if (e.getButton() == MouseEvent.BUTTON1 && e.getClickCount() == 2) {
+//					if (getNeptusIM() != null)
+//						getNeptusIM().setSelectedChannel(((IMChannel)getSelectedValue()).getRemoteID());
 				}
 				if(e.getButton() == MouseEvent.BUTTON3)
 				{
 				}
+				
+//				final IMChannel channel = (IMChannel)getSelectedValue();
+//				setToolTipText("<html>Channel name: <b>"+channel.getRemoteUsername()+"</b><br>"+
+//						"Channel ID: <b>"+channel.getRemoteID()+"</b><br></html>");
+//				
+//				if (e.getButton() == MouseEvent.BUTTON3 && !channel.getRemoteID().equals("ALL")) {
+//					JPopupMenu popup = new JPopupMenu();
+//					
+//					popup.add(new AbstractAction("Nudge "+channel.getRemoteUsername()) {
+//						public void actionPerformed(java.awt.event.ActionEvent e) {
+//							neptusIM.nudge(channel.getRemoteID());
+//						};
+//					});
+//					
+//					popup.show(ChannelsList.this, e.getX(), e.getY());
+//				}
 						
 			}
 		});
@@ -244,13 +265,15 @@ JXList provides api to access items of the underlying model in view coordinates 
 		return null;
 	}
 
+	
 	public static void main(String[] args) {
 		LogFolderInfoList list = new LogFolderInfoList();
 		list.addFolder(new LogFolderInfo("22/A"));
 		list.addFolder(new LogFolderInfo("12/A"));
 		list.addFolder(new LogFolderInfo("22/a"));
 		
-		list.setSortable(true); 
+		list.setSortable(true); //list.setFilterEnabled(true); // Changed from swingx 1.6.+ !!!!
+		//list.setComparator((Comparator<?>) new LogFolder("<><>"));
 		list.setAutoCreateRowSorter(true);
         list.setSortOrder(SortOrder.ASCENDING);
 		
