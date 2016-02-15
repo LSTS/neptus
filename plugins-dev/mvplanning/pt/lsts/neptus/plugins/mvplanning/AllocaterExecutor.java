ae2ba1bff757555482b8be1a19cfad49b49cd9a4
tsmarques <samarques.tiago@gmail.com>
2016-02-15 18:35:38 +0000

    mvplanning: Renamed 'AllocaterExecutor' to 'PlanAllocator'.

--- plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/AllocaterExecutor.java
+++
@@ -1,7 +0,0 @@
-package pt.lsts.neptus.plugins.mvplanning;
-
-/* Sends plans to the vehicles.
- Also send 'execution commands'*/
-public class AllocaterExecutor {
-
-}
---
+++ plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/PlanAllocator.java
@@ -0,0 +1,15 @@
+package pt.lsts.neptus.plugins.mvplanning;
+
+import pt.lsts.imc.PlanDB;
+import pt.lsts.imc.PlanDB.OP;
+import pt.lsts.neptus.comm.IMCSendMessageUtils;
+import pt.lsts.neptus.console.ConsoleLayout;
+import pt.lsts.neptus.i18n.I18n;
+
+/* Sends plans to the vehicles.
+ Also send 'execution commands'*/
+public class PlanAllocator {
+    public PlanAllocator() {
+        
+    }
+}
