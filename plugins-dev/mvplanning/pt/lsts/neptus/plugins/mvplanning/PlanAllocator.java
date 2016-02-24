bb515130acb6ef64f35412f8c8a2f6d45703966d
tsmarques <samarques.tiago@gmail.com>
2016-02-24 12:16:34 +0000

    mvplanning/PlanAllocator: Store a reference to the 'VehicleAwareness' module.

--- plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/PlanAllocator.java
+++ plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/PlanAllocator.java
@@ -5,11 +5,18 @@ import pt.lsts.imc.PlanDB.OP;
 import pt.lsts.neptus.comm.IMCSendMessageUtils;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.plugins.mvplanning.utils.VehicleAwareness;
 
 /* Sends plans to the vehicles.
  Also send 'execution commands'*/
 public class PlanAllocator {
-    public PlanAllocator() {
-        
+    private VehicleAwareness vawareness;
+    public PlanAllocator(VehicleAwareness vawareness) {
+        this.vawareness = vawareness;
+    }
+ 
+    public void allocate(PlanTask pTask) {
+        /* allocate plans to vehicle */
+        System.out.println("[mvplanning/PlanAlocater]: Allocating plan");
     }
 }
