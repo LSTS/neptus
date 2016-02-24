e351bdb482e5a4cd2b6bd4130f726b7810a588de
tsmarques <samarques.tiago@gmail.com>
2016-02-24 12:21:11 +0000

    mvplanning: Added basic implementation of PlanGenerator.

--- plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/PlanGenerator.java
+++ plugins-dev/mvplanning/pt/lsts/neptus/plugins/mvplanning/PlanGenerator.java
@@ -1,5 +1,23 @@
 package pt.lsts.neptus.plugins.mvplanning;
 
+import pt.lsts.imc.PlanSpecification;
+import pt.lsts.neptus.plugins.mvplanning.utils.jaxb.Profile;
 
 public class PlanGenerator {
+    private PlanAllocator planAloc;
+    public PlanGenerator(PlanAllocator planAloc) {
+        this.planAloc = planAloc;
+    }
+
+    public PlanGenerator() {
+
+    }
+
+    public void generatePlan(Profile planProfile, Object obj) {
+        if(obj.getClass().getSimpleName().equals("PlanSpecification"))
+            planAloc.allocate(new PlanTask((PlanSpecification) obj, planProfile, 0));
+        else {
+            System.out.println("[mvplanning/PlanGenerator]: Generating a plan");
+        }
+    }
 }
