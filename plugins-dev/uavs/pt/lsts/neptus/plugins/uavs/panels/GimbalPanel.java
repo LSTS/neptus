d6cd7dbb81e40dc025c5237608d0d8f91ca644e2
Manuel Ribeiro <manuelribeiro775@hotmail.com>
2016-03-03 18:08:08 +0000

    GimbalPanel: Pitch and roll values adjustment.

--- plugins-dev/uavs/pt/lsts/neptus/plugins/uavs/panels/GimbalPanel.java
+++ plugins-dev/uavs/pt/lsts/neptus/plugins/uavs/panels/GimbalPanel.java
@@ -120,11 +120,11 @@ public class GimbalPanel extends ConsolePanel implements MainVehicleChangeListen
         });
         
         Hashtable<Integer, JLabel> rollLblTable = new Hashtable<>();
-        rollLblTable.put( new Integer( -45 ),  new JLabel("-45"));
-        rollLblTable.put( new Integer( -15 ), new JLabel("-15") );
-        rollLblTable.put( new Integer( 0 ), new JLabel("0") );
-        rollLblTable.put( new Integer( 45 ),  new JLabel("45"));
-        rollLblTable.put( new Integer( 15 ), new JLabel("15") );
+        rollLblTable.put( new Integer(0),  new JLabel("-45"));
+        rollLblTable.put( new Integer(30), new JLabel("-15") );
+        rollLblTable.put( new Integer(90), new JLabel("0") ); 
+        rollLblTable.put( new Integer(180),  new JLabel("45"));
+        rollLblTable.put( new Integer(120), new JLabel("15") ); 
         rollSlider.setLabelTable(rollLblTable);
 
         buttonPanel.add(rollSlider, "w 50%, h 100%");
@@ -133,13 +133,13 @@ public class GimbalPanel extends ConsolePanel implements MainVehicleChangeListen
         pitchSlider.setPaintTicks(true);
         
         Hashtable<Integer, JLabel> pitchLblTable = new Hashtable<>();
-        pitchLblTable.put( new Integer( -90 ),  new JLabel("-90 - DWN"));
-        pitchLblTable.put( new Integer( -45 ), new JLabel("-45") );
-        pitchLblTable.put( new Integer( -15 ), new JLabel("-15") );
-        pitchLblTable.put( new Integer( 0 ), new JLabel("0 - FWD") );
-        pitchLblTable.put( new Integer( 15 ), new JLabel("15") );
-        pitchLblTable.put( new Integer( 30 ), new JLabel("30") );
-        pitchLblTable.put( new Integer( 45 ), new JLabel("45 - UP") );
+        pitchLblTable.put( new Integer(0),  new JLabel("-90 - DWN"));
+        pitchLblTable.put( new Integer(30), new JLabel("-45") );
+        pitchLblTable.put( new Integer(60), new JLabel("-15") );
+        pitchLblTable.put( new Integer(90), new JLabel("0 - FWD") );
+        pitchLblTable.put( new Integer(120), new JLabel("15") );
+        pitchLblTable.put( new Integer(150), new JLabel("30") );
+        pitchLblTable.put( new Integer(180), new JLabel("45 - UP") );
         pitchSlider.setLabelTable( pitchLblTable );
         pitchSlider.setPaintLabels(true);
         
@@ -147,9 +147,8 @@ public class GimbalPanel extends ConsolePanel implements MainVehicleChangeListen
             @Override
             public void stateChanged(ChangeEvent e) {
                 JSlider source = (JSlider)e.getSource();
-//                int pos2 = (int) source.getValue();
 
-//                if (!source.getValueIsAdjusting()) {
+                if (!source.getValueIsAdjusting()) {
                     int pos = (int) source.getValue();
                     SetServoPosition pitchMsg = new SetServoPosition();
                     pitchMsg.setId(PITCH_SERVO_ID);
@@ -158,7 +157,7 @@ public class GimbalPanel extends ConsolePanel implements MainVehicleChangeListen
                     pitchMsg.setValue(rads);
                     
                     send(pitchMsg);
-                //}      
+                }      
             }
         });
         
