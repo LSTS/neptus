963aa8320476e5475b4c59a0dcdfd3911f77259e
Jose Pinto <zepinto@gmail.com>
2016-04-12 11:21:11 +0100

    StationKeeping minimum radius is now 0. Fixes #3399.

--- src/pt/lsts/neptus/mp/maneuvers/StationKeeping.java
+++ src/pt/lsts/neptus/mp/maneuvers/StationKeeping.java
@@ -62,10 +62,10 @@ import com.l2fprod.common.propertysheet.Property;
 public class StationKeeping extends Maneuver implements LocatedManeuver, IMCSerialization, StatisticsProvider {
 
 	public static final int INFINITY_DURATION = 0;
-	public static final double MINIMUM_SK_RADIUS = 20;
+	public static final double MINIMUM_SK_RADIUS = 0;
 	
 	private int duration = 60;
-	private double radius = MINIMUM_SK_RADIUS, speed = 30;
+	private double radius = 20, speed = 30;
 	private String speedUnits = "m/s";
 	private ManeuverLocation location = new ManeuverLocation();	 
 
