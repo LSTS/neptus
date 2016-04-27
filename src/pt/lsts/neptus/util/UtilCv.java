269c38a64f3f6e1bb323dd12ab6e5d8d0b17afcd
Paulo Dias <paulo.sousa.dias@gmail.com>
2016-04-27 12:46:25 +0200

    util/UtilCv: Cleanup.

--- src/pt/lsts/neptus/util/UtilCv.java
+++ src/pt/lsts/neptus/util/UtilCv.java
@@ -54,7 +54,7 @@ import org.opencv.imgproc.Imgproc;
 import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.i18n.I18n;
 
-/** 
+/**
  * @author pedrog
  * @version 1.0
  * @category OpenCV-Vision
@@ -64,55 +64,54 @@ public class UtilCv {
 
     private UtilCv() {
     }
-    
-    //!Convert a Mat image to bufferedImage
+
+    /**  Convert a Mat image to bufferedImage */
     public static BufferedImage matToBufferedImage(Mat matrix) {
-        int cols = matrix.cols();  
-        int rows = matrix.rows();  
-        int elemSize = (int)matrix.elemSize();  
-        byte[] data = new byte[cols * rows * elemSize];  
-        int type;  
+        int cols = matrix.cols();
+        int rows = matrix.rows();
+        int elemSize = (int) matrix.elemSize();
+        byte[] data = new byte[cols * rows * elemSize];
+        int type;
         matrix.get(0, 0, data);
-        switch (matrix.channels()) {  
-            case 1:  
-                type = BufferedImage.TYPE_BYTE_GRAY;  
-                break;  
-            case 3:  
-                type = BufferedImage.TYPE_3BYTE_BGR;  
-                // bgr to rgb  
-                byte b;  
-                for(int i=0; i<data.length; i=i+3) {  
-                    b = data[i];  
-                    data[i] = data[i+2];  
-                    data[i+2] = b;  
-                }  
-                break;  
-        default:  
-            return null;  
+        switch (matrix.channels()) {
+            case 1:
+                type = BufferedImage.TYPE_BYTE_GRAY;
+                break;
+            case 3:
+                type = BufferedImage.TYPE_3BYTE_BGR;
+                // bgr to rgb
+                byte b;
+                for (int i = 0; i < data.length; i = i + 3) {
+                    b = data[i];
+                    data[i] = data[i + 2];
+                    data[i + 2] = b;
+                }
+                break;
+            default:
+                return null;
         }
-        BufferedImage image2 = new BufferedImage(cols, rows, type);  
-        image2.getRaster().setDataElements(0, 0, cols, rows, data);  
+        BufferedImage image2 = new BufferedImage(cols, rows, type);
+        image2.getRaster().setDataElements(0, 0, cols, rows, data);
         return image2;
     }
-    
-    //!Convert bufferedImage to Mat
+
+    /**  Convert bufferedImage to Mat */
     public static Mat bufferedImageToMat(BufferedImage in) {
         Mat out;
 
-        if(in.getType() == BufferedImage.TYPE_3BYTE_BGR) {
+        if (in.getType() == BufferedImage.TYPE_3BYTE_BGR) {
             out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC3);
             byte[] pixels = ((DataBufferByte) in.getRaster().getDataBuffer()).getData();
             out.put(0, 0, pixels);
         }
-        else if(in.getType() == BufferedImage.TYPE_INT_RGB) {
+        else if (in.getType() == BufferedImage.TYPE_INT_RGB) {
             out = new Mat(in.getHeight(), in.getWidth(), CvType.CV_8UC3);
-            byte [] data = new byte[in.getWidth() * in.getHeight() * (int)out.elemSize()];
+            byte[] data = new byte[in.getWidth() * in.getHeight() * (int) out.elemSize()];
             int[] dataBuff = in.getRGB(0, 0, in.getWidth(), in.getHeight(), null, 0, in.getWidth());
-            for(int i = 0; i < dataBuff.length; i++)
-            {
-                data[i*3] = (byte) ((dataBuff[i] >> 16) & 0xFF);
-                data[i*3 + 1] = (byte) ((dataBuff[i] >> 8) & 0xFF);
-                data[i*3 + 2] = (byte) ((dataBuff[i] >> 0) & 0xFF);
+            for (int i = 0; i < dataBuff.length; i++) {
+                data[i * 3] = (byte) ((dataBuff[i] >> 16) & 0xFF);
+                data[i * 3 + 1] = (byte) ((dataBuff[i] >> 8) & 0xFF);
+                data[i * 3 + 2] = (byte) ((dataBuff[i] >> 0) & 0xFF);
             }
             out.put(0, 0, data);
         }
@@ -122,10 +121,10 @@ public class UtilCv {
             out.put(0, 0, pixels);
         }
         return out;
-     }
-    
-    //!Resize Buffered Image
-    public static BufferedImage resize(BufferedImage img, int newW, int newH) { 
+    }
+
+    /**  Resize Buffered Image */
+    public static BufferedImage resize(BufferedImage img, int newW, int newH) {
         Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
         BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
 
@@ -135,17 +134,17 @@ public class UtilCv {
 
         return dimg;
     }
-    
-    //!Add text to Buffered Image
+
+    /**  Add text to Buffered Image */
     public static BufferedImage addText(BufferedImage old, String text, Color m_color, int posX, int posY) {
-        BufferedImage img = new BufferedImage( old.getWidth(), old.getHeight(), BufferedImage.TYPE_INT_ARGB);
+        BufferedImage img = new BufferedImage(old.getWidth(), old.getHeight(), BufferedImage.TYPE_INT_ARGB);
         Graphics2D g1d = img.createGraphics();
         g1d.drawImage(old, 0, 0, null);
         g1d.setPaint(Color.DARK_GRAY);
         g1d.setFont(new Font("Serif", Font.BOLD, 14));
         FontMetrics fm = g1d.getFontMetrics();
         g1d.drawString(text, posX - fm.stringWidth(text) - 10, posY);
-        
+
         g1d.setPaint(m_color);
         g1d.setFont(new Font("Serif", Font.BOLD, 14));
         fm = g1d.getFontMetrics();
@@ -153,19 +152,19 @@ public class UtilCv {
         g1d.dispose();
         return img;
     }
-        
-    //!Histogram equalizer
+
+    /**  Histogram equalizer */
     public static BufferedImage histogramCv(BufferedImage original) {
         Mat matGray = null;
         Mat matColor = null;
         Mat mR = null;
         Mat mG = null;
         Mat mB = null;
-        if(original.getWidth() > 0 && original.getHeight() > 0) {
-            tmp = original;
+        if (original.getWidth() > 0 && original.getHeight() > 0) {
             BufferedImage tmp = original;
             try {
-                if(original.getType() == BufferedImage.TYPE_INT_RGB || original.getType() == BufferedImage.TYPE_3BYTE_BGR) {
+                if (original.getType() == BufferedImage.TYPE_INT_RGB
+                        || original.getType() == BufferedImage.TYPE_3BYTE_BGR) {
                     matColor = new Mat(original.getHeight(), original.getWidth(), CvType.CV_8UC3);
                     List<Mat> lRgb = new ArrayList<Mat>(3);
                     Core.split(bufferedImageToMat(original), lRgb);
@@ -185,40 +184,40 @@ public class UtilCv {
                     mG.release();
                     mB.release();
                 }
-                else if(original.getType() == BufferedImage.TYPE_BYTE_GRAY) {
+                else if (original.getType() == BufferedImage.TYPE_BYTE_GRAY) {
                     matGray = new Mat(original.getHeight(), original.getWidth(), CvType.CV_8UC1);
                     Imgproc.equalizeHist(matGray, matGray);
                     original = matToBufferedImage(matGray);
                     matGray.release();
                 }
             }
-            catch(Exception e){
-                NeptusLog.pub().warn(I18n.text("Histogram equalizer ERROR: ")+e.getMessage());
-                if(matColor != null)
+            catch (Exception e) {
+                NeptusLog.pub().warn(I18n.text("Histogram equalizer ERROR: ") + e.getMessage());
+                if (matColor != null)
                     matColor.release();
-                if(mR != null)
+                if (mR != null)
                     mR.release();
-                if(mG != null)
+                if (mG != null)
                     mG.release();
-                if(mB != null)
+                if (mB != null)
                     mB.release();
-                if(matGray != null)
+                if (matGray != null)
                     matGray.release();
-                
+
                 return tmp;
             }
         }
         else
             NeptusLog.pub().warn(I18n.text("Histogram equalizer ERROR"));
-        
+
         return original;
     }
-    
-    //!Save a snapshot to disk
+
+    /**  Save a snapshot to disk */
     public static void saveSnapshot(BufferedImage image, String snapshotdir) {
         Date date = new Date();
         String dateFolder = String.format("%tT", date);
-        String imageJpeg = String.format("%s/%s.png",snapshotdir , dateFolder.replace(":", "-"));
+        String imageJpeg = String.format("%s/%s.png", snapshotdir, dateFolder.replace(":", "-"));
         File outputfile = new File(imageJpeg);
         try {
             ImageIO.write(image, "png", outputfile);
