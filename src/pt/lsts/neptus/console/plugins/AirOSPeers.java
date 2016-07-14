dcc77048b4a73a9022616f018358b0027d9efbe0
Jose Pinto <zepinto@gmail.com>
2016-07-14 10:03:14 +0100

    AirOsPeers: Fixed dumping of TimeoutExceptions to console.

--- src/pt/lsts/neptus/console/plugins/AirOSPeers.java
+++ src/pt/lsts/neptus/console/plugins/AirOSPeers.java
@@ -42,6 +42,7 @@ import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import javax.swing.JFileChooser;
 
@@ -149,6 +150,9 @@ public class AirOSPeers extends ConsolePanel {
                 if (ts != null)
                     ts.addOrUpdate(new Millisecond(new Date(System.currentTimeMillis())), null);
             }
+        }
+        catch (TimeoutException timeout) {
+            NeptusLog.pub().error("Timeout while trying to connect to "+host+":"+port);
         }
         catch (Exception e) {
             e.printStackTrace();
