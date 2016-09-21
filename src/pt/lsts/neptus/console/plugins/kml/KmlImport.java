38605885f20f0682c7490af608e2d26113884c0f
Paulo Dias <paulo.sousa.dias@gmail.com>
2016-09-21 18:08:46 +0100

    console/plugins/kml: Adding icon.

--- src/pt/lsts/neptus/console/plugins/kml/KmlImport.java
+++ src/pt/lsts/neptus/console/plugins/kml/KmlImport.java
@@ -101,10 +101,11 @@ import pt.lsts.neptus.util.conf.ConfigFetch;
 
 /**
  * @author tsmarques
- *
+ * @author pdias
  */
 @SuppressWarnings("serial")
-@PluginDescription(name = "Kml Import", description = "Import map features from KML, from a file or URL", author = "tsmarques", version = "0.1")
+@PluginDescription(name = "Kml Import", description = "Import map features from KML, from a file or URL", 
+    author = "tsmarques", version = "0.6", icon = "pt/lsts/neptus/console/plugins/kml/kml-icon.png")
 @Popup(name = "Kml Import", pos = POSITION.CENTER, width = 230, height = 500)
 @LayerPriority(priority = 50)
 public class KmlImport extends ConsolePanel {
