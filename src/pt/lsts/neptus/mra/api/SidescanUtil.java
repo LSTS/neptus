bacea9725bbf2446961b315feb7c081f637e04c6
Paulo Dias <paulo.sousa.dias@gmail.com>
2016-11-11 11:55:44 +0000

    mra/api/SidescanGuiUtils: Fix reading data with more than 16 bits (24 and 32).

--- src/pt/lsts/neptus/mra/api/SidescanUtil.java
+++ src/pt/lsts/neptus/mra/api/SidescanUtil.java
@@ -217,13 +217,13 @@ public class SidescanUtil {
         double[] fData = new double[data.length / bytesPerPoint];
         
         int k = 0;
-        for (int i = 0; i < data.length; i++) {
+        for (int i = 0; i < data.length; /* i incremented inside the 2nd loop */) {
             double val = 0;
             for (int j = 0; j < bytesPerPoint; j++) {
-                i = i + j; // progressing index of data
                 int v = data[i] & 0xFF;
                 v = (v << 8 * j);
                 val += v;
+                i++; // progressing index of data
             }
             fData[k++] = val;
         }
