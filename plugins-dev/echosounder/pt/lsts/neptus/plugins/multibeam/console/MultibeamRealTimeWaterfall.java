0a05f456f1f74b3f78290ef9635502080a79126e
tsmarques <samarques.tiago@gmail.com>
2016-11-12 16:01:44 +0100

    multibeam/console/MultibeamRealTimeWaterfall: Removed test cases.

--- plugins-dev/echosounder/pt/lsts/neptus/plugins/multibeam/console/MultibeamRealTimeWaterfall.java
+++ plugins-dev/echosounder/pt/lsts/neptus/plugins/multibeam/console/MultibeamRealTimeWaterfall.java
@@ -104,7 +104,7 @@ public class MultibeamRealTimeWaterfall extends ConsolePanel implements Configur
     public MultibeamRealTimeWaterfall(ConsoleLayout console) {
         super(console);
         initialize();
-        testDataDisplay();
+/*        testDataDisplay();*/
     }
 
 
