71d79dc20cac572ae1ab772c0f1ac008c7236977
Manuel R <manuelribeiro@gmail.com>
2016-11-15 17:15:27 +0000

    UAV-Parameters: Retry requesting parameters.

--- plugins-dev/uav-parameters/pt/lsts/neptus/plugins/uavparameters/ParameterManager.java
+++ plugins-dev/uav-parameters/pt/lsts/neptus/plugins/uavparameters/ParameterManager.java
@@ -38,7 +38,8 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -72,28 +73,26 @@ import pt.lsts.neptus.plugins.uavparameters.connection.MAVLinkConnectionListener
 @PluginDescription(name = "UAV Parameter Configuration", icon = "images/settings2.png")
 @Popup(name = "UAV Parameter Configuration Panel", pos = POSITION.CENTER, height = 500, width = 400, accelerator = '0')
 public class ParameterManager extends ConsolePanel implements MainVehicleChangeListener, MAVLinkConnectionListener {
+    private static final int TIMEOUT = 15000;
+    private static final int RETRYS = 3;
     private JTextField findTxtField;
     private JTable table;
     private MAVLinkConnection mavlink = null;
+    private boolean success = false;
+    
     private int expectedParams;
-    private final HashMap<Integer, Parameter> parameters = new HashMap<Integer, Parameter>();
-    public final ArrayList<Parameter> parameterList = new ArrayList<Parameter>();
-
+    private HashMap<Integer, Parameter> parameters = new HashMap<Integer, Parameter>();
+    private ArrayList<Parameter> parameterList = new ArrayList<Parameter>();
+    
+    class ParamReqTask implements Runnable
+    {
+        public void run() { 
+            onParameterStreamStopped();
+        }
+    }
+    
     public ParameterManager(ConsoleLayout console) {
         super(console);
-    }
-
-    @Override
-    public void cleanSubPanel() {
-        if (mavlink != null) {
-            try {
-                mavlink.closeConnection();
-            }
-            catch (IOException e) {
-                e.printStackTrace();
-            }
-            
-        }
     }
     
     @Override
@@ -121,9 +120,34 @@ public class ParameterManager extends ConsolePanel implements MainVehicleChangeL
             @Override
             public void actionPerformed(ActionEvent e) {
                 SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+
                     @Override
                     protected Void doInBackground() throws Exception {
-                        refreshParameters();
+                        int num_of_retries = 1;
+                        long now = System.currentTimeMillis();
+                        requestParameters();
+                        
+                        while (num_of_retries <= RETRYS && !success) {
+                            while(((System.currentTimeMillis() - now) < TIMEOUT) && !success)
+                            {
+                                Thread.sleep(1000);
+                                System.out.println("...");
+                                
+                            }
+                            if (!success)
+                                onParameterStreamStopped();
+                            
+                            now = System.currentTimeMillis();
+                            num_of_retries++;
+                        }
+                        
+                        if (expectedParams != getParametersList().size()) {
+                            System.out.println("FAIL: Unable to load all parameters");
+                           
+                        }
+                        else {
+                            System.out.println("SUCCESS: All parameters loaded");
+                        }
                         return null;
                     }
                 };
@@ -175,17 +199,15 @@ public class ParameterManager extends ConsolePanel implements MainVehicleChangeL
     }
 
     
-    public void refreshParameters() {
+    public void requestParameters() {
         parameters.clear();
         parameterList.clear();
+        success = false;
 
         if (mavlink != null)
             MAVLinkParameters.requestParametersList(mavlink);
     }
     
-    public List<Parameter> getParametersList(){
-        return parameterList;
-    }
     
     private boolean processMessage(MAVLinkMessage msg) {
         if (msg.msgid == msg_param_value.MAVLINK_MSG_ID_PARAM_VALUE) {
@@ -196,19 +218,33 @@ public class ParameterManager extends ConsolePanel implements MainVehicleChangeL
     }
 
     private void processReceivedParam(msg_param_value m_value) {
-        // collect params in parameter list
         Parameter param = new Parameter(m_value);
         parameters.put((int) m_value.param_index, param);
 
         expectedParams = m_value.param_count;
-
-        // Are all parameters here? Notify the listener with the parameters
+        
+        SwingWorker<Void, Void> worker = new SwingWorker<Void, Void>() {
+            @Override
+            protected Void doInBackground() throws Exception {
+                findTxtField.setText(parameters.size()+ " of " + expectedParams);
+                return null;
+            }
+        };
+        worker.execute();
+        
+        //All parameters here!
         if (parameters.size() >= m_value.param_count) {
             parameterList.clear();
             for (int key : parameters.keySet()) {
                 parameterList.add(parameters.get(key));
             }
+            
+            success = true;
         }
+    }
+    
+    private void onParameterStreamStopped() {
+        reRequestMissingParams(expectedParams);
     }
     
     private void reRequestMissingParams(int howManyParams) {
@@ -230,4 +266,20 @@ public class ParameterManager extends ConsolePanel implements MainVehicleChangeL
         processMessage(msg);
     }
 
+    public List<Parameter> getParametersList(){
+        return parameterList;
+    }
+    
+    @Override
+    public void cleanSubPanel() {
+        if (mavlink != null) {
+            try {
+                mavlink.closeConnection();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+        }
+    }
 }
