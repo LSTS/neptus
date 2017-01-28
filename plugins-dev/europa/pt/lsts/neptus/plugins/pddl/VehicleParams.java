40c6e897787394cacc392c4e197074aa2914ec17
zp <zp@lsts-asus-2>
2017-01-28 14:23:32 +0000

    Updated vehicle params.

--- plugins-dev/europa/pt/lsts/neptus/plugins/pddl/VehicleParams.java
+++ plugins-dev/europa/pt/lsts/neptus/plugins/pddl/VehicleParams.java
@@ -45,28 +45,21 @@ public class VehicleParams {
     
     private static LinkedHashMap<String, Double> batteryCapacities = new LinkedHashMap<String, Double>();
     static {
-        batteryCapacities.put("lauv-dolphin-1", 700.0);
-        batteryCapacities.put("lauv-dolphin-2", 1050.0);
-        batteryCapacities.put("lauv-dolphin-3", 1050.0);
-        batteryCapacities.put("lauv-oceaneco-1", 700.0);
-        batteryCapacities.put("lauv-oceaneco-2", 525.0);
         batteryCapacities.put("lauv-seacon-1", 700.0);
         batteryCapacities.put("lauv-seacon-2", 525.0);
         batteryCapacities.put("lauv-seacon-3", 525.0);
         batteryCapacities.put("lauv-xtreme-2", 525.0);
         batteryCapacities.put("lauv-xplore-1", 1400.0);
+        batteryCapacities.put("lauv-xplore-2", 1400.0);        
         batteryCapacities.put("lauv-noptilus-1", 700.0);
         batteryCapacities.put("lauv-noptilus-2", 700.0);
         batteryCapacities.put("lauv-noptilus-3", 700.0);
-        batteryCapacities.put("lauv-lsts-1", 700.0);
-        batteryCapacities.put("lauv-lupis-1", 525.0);
     }
     
     private static LinkedHashMap<String, String> nicknames = new LinkedHashMap<String, String>();
     static {
-        String[] vehicles = new String[] { "lauv-dolphin-1", "lauv-dolphin-2", "lauv-dolphin-3", "lauv-oceaneco-1",
-                "lauv-oceaneco-2", "lauv-seacon-1", "lauv-seacon-2", "lauv-seacon-3", "lauv-xtreme-2", "lauv-xplore-1",
-                "lauv-noptilus-1", "lauv-noptilus-2", "lauv-noptilus-3", "lauv-lsts-1", "lauv-lupis-1" };
+        String[] vehicles = new String[] {"lauv-seacon-1", "lauv-seacon-2", "lauv-seacon-3", "lauv-xtreme-2", "lauv-xplore-1",
+                "lauv-xplore-2", "lauv-noptilus-1", "lauv-noptilus-2", "lauv-noptilus-3" };
         
         for (String v : vehicles) {
             nicknames.put(VehiclesHolder.getVehicleById(v).getNickname(), v);
@@ -79,49 +72,34 @@ public class VehicleParams {
     
     private static LinkedHashMap<String, Double> moveConsumption = new LinkedHashMap<String, Double>();
     static {
-        moveConsumption.put("lauv-dolphin-1", 14.0);
-        moveConsumption.put("lauv-dolphin-2", 14.0);
-        moveConsumption.put("lauv-dolphin-3", 14.0);
-        moveConsumption.put("lauv-oceaneco-1", 14.0);
-        moveConsumption.put("lauv-oceaneco-2", 14.0);
         moveConsumption.put("lauv-seacon-1", 14.0);
         moveConsumption.put("lauv-seacon-2", 14.0);
         moveConsumption.put("lauv-seacon-3", 14.0);
         moveConsumption.put("lauv-xtreme-2", 14.0);
         moveConsumption.put("lauv-xplore-1", 14.0);
+        moveConsumption.put("lauv-xplore-2", 14.0);
         moveConsumption.put("lauv-noptilus-1", 14.0);
         moveConsumption.put("lauv-noptilus-2", 14.0);
         moveConsumption.put("lauv-noptilus-3", 14.0);
-        moveConsumption.put("lauv-lsts-1", 14.0);
-        moveConsumption.put("lauv-lupis-1", 14.0);
     }
 
 
     private static LinkedHashMap<String, PayloadRequirement[]> payloads = new LinkedHashMap<String, PayloadRequirement[]>();
     static {
-        payloads.put("lauv-dolphin-1", new PayloadRequirement[] { PayloadRequirement.edgetech,
-                PayloadRequirement.camera });
-        payloads.put("lauv-dolphin-2", new PayloadRequirement[] { PayloadRequirement.edgetech,
-                PayloadRequirement.camera });
-        payloads.put("lauv-dolphin-3", new PayloadRequirement[] { PayloadRequirement.edgetech,
-                PayloadRequirement.camera });
-        payloads.put("lauv-oceaneco-1", new PayloadRequirement[] { PayloadRequirement.sidescan });
-        payloads.put("lauv-oceaneco-2", new PayloadRequirement[] { PayloadRequirement.ctd });
         payloads.put("lauv-seacon-1", new PayloadRequirement[] { PayloadRequirement.sidescan,
                 PayloadRequirement.camera });
         payloads.put("lauv-seacon-2", new PayloadRequirement[] { PayloadRequirement.ctd });
         payloads.put("lauv-seacon-3", new PayloadRequirement[] { PayloadRequirement.ctd });
         payloads.put("lauv-xtreme-2", new PayloadRequirement[] { PayloadRequirement.ctd,
                 PayloadRequirement.sidescan, PayloadRequirement.camera });
-        payloads.put("lauv-xplore-1", new PayloadRequirement[] { PayloadRequirement.ctd,
+        payloads.put("lauv-xplore-1", new PayloadRequirement[] { PayloadRequirement.ctd });
+        payloads.put("lauv-xplore-2", new PayloadRequirement[] { PayloadRequirement.ctd,
                 PayloadRequirement.rhodamine });
         payloads.put("lauv-noptilus-1", new PayloadRequirement[] { PayloadRequirement.sidescan,
                 PayloadRequirement.multibeam, PayloadRequirement.ctd });
         payloads.put("lauv-noptilus-2", new PayloadRequirement[] { PayloadRequirement.edgetech });
         payloads.put("lauv-noptilus-3", new PayloadRequirement[] { PayloadRequirement.sidescan,
                 PayloadRequirement.multibeam, PayloadRequirement.camera });
-        payloads.put("lauv-lsts-1", new PayloadRequirement[] {});
-        payloads.put("lauv-lupis-1", new PayloadRequirement[] { PayloadRequirement.rhodamine });
     }
     
     public static PayloadRequirement[] payloadsFor(VehicleType vehicle) {
