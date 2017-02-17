2280f97c135ed2c66cc0b131d4b594ff84c68488
Manuel R <manuelribeiro@gmail.com>
2017-02-17 17:29:11 +0000

    MRA/MarkerManagement: Generate color based on Classification type.

--- src/pt/lsts/neptus/mra/markermanagement/exporters/HTMLExporter.java
+++ src/pt/lsts/neptus/mra/markermanagement/exporters/HTMLExporter.java
@@ -54,6 +54,7 @@ import com.webfirmframework.wffweb.tag.html.attribute.Width;
 import com.webfirmframework.wffweb.tag.html.attribute.global.Style;
 import com.webfirmframework.wffweb.tag.html.attributewff.CustomAttribute;
 import com.webfirmframework.wffweb.tag.html.formatting.Strong;
+import com.webfirmframework.wffweb.tag.html.html5.stylesandsemantics.Footer;
 import com.webfirmframework.wffweb.tag.html.images.Img;
 import com.webfirmframework.wffweb.tag.html.metainfo.Head;
 import com.webfirmframework.wffweb.tag.html.stylesandsemantics.Span;
@@ -67,6 +68,7 @@ import com.webfirmframework.wffweb.tag.htmlwff.CustomTag;
 import com.webfirmframework.wffweb.tag.htmlwff.NoTag;
 
 import pt.lsts.neptus.mra.markermanagement.LogMarkerItem;
+import pt.lsts.neptus.mra.markermanagement.LogMarkerItem.Classification;
 import pt.lsts.neptus.util.GuiUtils;
 
 /**
@@ -87,15 +89,6 @@ public class HTMLExporter {
         final Style style11 = new Style("white-space:nowrap;");
         final Style style14 = new Style("text-align: left;");
         final Style whiteStyle = new Style("color: white;");
-        final String[] classifColorStyle = new String[7];
-        classifColorStyle[0] = new String("color: BlueViolet;"); //UNDEFINED
-        classifColorStyle[1] = new String("color: Brown;"); //NONE
-        classifColorStyle[2] = new String("color: Chocolate;"); //CABLE
-        classifColorStyle[3] = new String("color: Crimson;"); //PIPE
-        classifColorStyle[4] = new String("color: DarkSalmon;"); //ROCK
-        classifColorStyle[5] = new String("color: DarkSeaGreen;"); //WRECK
-        classifColorStyle[6] = new String("color: MediumOrchid;"); //UNKNOWN
-
         final Width width3 = new Width("600");
         final CustomAttribute customAttribute4 = new CustomAttribute("bgcolor", "#4A7FCF");
         final CustomAttribute customAttribute7 = new CustomAttribute("bgcolor", "#E6EEFF");
@@ -110,7 +103,11 @@ public class HTMLExporter {
                             + "td, th {\r\n    "
                             + "border: 1px solid black;\r\n"
                             + "padding: 3px;\r\n"
-                            + "}");
+                            + "}\r\n\r\n"
+                            + "@media print {\r\n"
+                            + "footer {"
+                            + "page-break-after: always;"
+                            + "}\r\n}");
                 }};
             }};
             new Body(this) {{
@@ -128,90 +125,92 @@ public class HTMLExporter {
                     String classif = toExportList.get(i).getClassification().toString();
                     String annot = toExportList.get(i).getAnnotation();
                     String img = toExportList.get(i).getSidescanImgPath().replaceAll("^/+", "");
-                    
-                    new Table(this, style2, width3) {{
-                        new THead(this) {{
-                            new Tr(this, customAttribute4) {{
-                                new Td(this, align5) {{
-                                    new CustomTag("font", this, whiteStyle) {{
-                                        new NoTag(this, "Image");
-                                    }};
-                                }};
-                                new Td(this, align5) {{
-                                    new CustomTag("font", this, whiteStyle) {{
-                                        new NoTag(this, "Details");
-                                    }};
-                                }};
-                            }};
-                        }};
-                        new TBody(this) {{
-                            new Tr(this, customAttribute7) {{
-                                new Td(this) {{
-                                    new Img(this,
-                                            new Src(img),
-                                            new Alt(lbl),
-                                            style10);
-                                }};
-                                new Td(this, style11) {{
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Label:");
+
+                    new Footer(this) {{
+                        new Table(this, style2, width3) {{
+                            new THead(this) {{
+                                new Tr(this, customAttribute4) {{
+                                    new Td(this, align5) {{
+                                        new CustomTag("font", this, whiteStyle) {{
+                                            new NoTag(this, "Image");
                                         }};
                                     }};
-                                    new P(this) {{
-                                        new NoTag(this, lbl);
-                                    }};
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Date:");
+                                    new Td(this, align5) {{
+                                        new CustomTag("font", this, whiteStyle) {{
+                                            new NoTag(this, "Details");
                                         }};
-                                    }};
-                                    new P(this) {{
-                                        new NoTag(this, ts);
-                                    }};
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Location:");
-                                        }};
-                                    }};
-                                    new P(this) {{
-                                        new NoTag(this, loc);
-                                    }};
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Depth:");
-                                        }};
-                                    }};
-                                    new P(this) {{
-                                        new NoTag(this, Double.toString(depth));
                                     }};
                                 }};
                             }};
-                            new Tr(this) {{
-                                new Td(this, colSpan12, align5) {{
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Classification:");
-                                        }};
+                            new TBody(this) {{
+                                new Tr(this, customAttribute7) {{
+                                    new Td(this) {{
+                                        new Img(this,
+                                                new Src(img),
+                                                new Alt(lbl),
+                                                style10);
                                     }};
-                                    new H2(this) {{
-                                        new Span(this, new Style(classifColorStyle[0])) {{
+                                    new Td(this, style11) {{
+                                        new P(this) {{
                                             new Strong(this) {{
-                                                new NoTag(this, classif);
+                                                new NoTag(this, "Label:");
+                                            }};
+                                        }};
+                                        new P(this) {{
+                                            new NoTag(this, lbl);
+                                        }};
+                                        new P(this) {{
+                                            new Strong(this) {{
+                                                new NoTag(this, "Date:");
+                                            }};
+                                        }};
+                                        new P(this) {{
+                                            new NoTag(this, ts);
+                                        }};
+                                        new P(this) {{
+                                            new Strong(this) {{
+                                                new NoTag(this, "Location:");
+                                            }};
+                                        }};
+                                        new P(this) {{
+                                            new NoTag(this, loc);
+                                        }};
+                                        new P(this) {{
+                                            new Strong(this) {{
+                                                new NoTag(this, "Depth:");
+                                            }};
+                                        }};
+                                        new P(this) {{
+                                            new NoTag(this, Double.toString(depth) + " m");
+                                        }};
+                                    }};
+                                }};
+                                new Tr(this) {{
+                                    new Td(this, colSpan12, align5) {{
+                                        new P(this) {{
+                                            new Strong(this) {{
+                                                new NoTag(this, "Classification:");
+                                            }};
+                                        }};
+                                        new H2(this) {{
+                                            new Span(this, generateColors(classif)) {{
+                                                new Strong(this) {{
+                                                    new NoTag(this, classif);
+                                                }};
                                             }};
                                         }};
                                     }};
                                 }};
-                            }};
-                            new Tr(this) {{
-                                new Td(this, colSpan12, align5) {{
-                                    new P(this) {{
-                                        new Strong(this) {{
-                                            new NoTag(this, "Annotation:");
+                                new Tr(this) {{
+                                    new Td(this, colSpan12, align5) {{
+                                        new P(this) {{
+                                            new Strong(this) {{
+                                                new NoTag(this, "Annotation:");
+                                            }};
                                         }};
-                                    }};
-                                    new P(this, style14) {{
-                                        new NoTag(this, annot);
+                                        new P(this, style14) {{
+                                            new NoTag(this, annot);
+                                        }};
                                     }};
                                 }};
                             }};
@@ -225,6 +224,39 @@ public class HTMLExporter {
 
         return html;
 
+    }
+
+    private static Style generateColors(String classification) {
+        Style style = new Style();
+        Classification temp = Classification.valueOf(classification);
+
+        switch (temp) {
+            case UNDEFINED: 
+                style.addCssProperties("color: BlueViolet;");
+                break;
+            case CABLE:
+                style.addCssProperties("color: Chocolate;");
+                break;
+            case NONE:
+                style.addCssProperties("color: Brown;");
+                break;
+            case PIPE:
+                style.addCssProperties("color: Crimson;");
+                break;
+            case ROCK:
+                style.addCssProperties("color: DarkSalmon;");
+                break;
+            case UNKNOWN:
+                style.addCssProperties("color: MediumOrchid;");
+                break;
+            case WRECK:
+                style.addCssProperties("color: DarkSeaGreen;");
+                break;
+            default:
+                break;
+        }
+
+        return style;
     }
 
     public static void saveHTML(Html createHTML, String fileName) {
