f479f9c0a74a968c2402eb8dc4df52332615054b
Paulo Dias <paulo.sousa.dias@gmail.com>
2017-03-09 17:48:28 +0000

    comm/manager/imc/ImcSystem: It is always need a comm mean setup.

--- src/pt/lsts/neptus/comm/manager/imc/ImcSystem.java
+++ src/pt/lsts/neptus/comm/manager/imc/ImcSystem.java
@@ -115,14 +115,15 @@ public class ImcSystem implements Comparable<ImcSystem> {
 		setId((tmpId == null)?ImcId16.NULL_ID:tmpId);
 		CommMean commMean = CommUtil.getActiveCommMeanForProtocol(
                 vehicle, CommMean.IMC);
-		setCommsInfo((commMean == null)?null:commMean);
+        setCommsInfo((commMean == null) ? createEmptyCommMean() : commMean);
 	}
 
-	/**
+    /**
 	 * @param id
 	 */
 	public ImcSystem(ImcId16 id) {
 		this((id == null)?ImcId16.NULL_ID.toString():id.toString(), id);
+        setCommsInfo(createEmptyCommMean());
 	}
 
 	/**
@@ -133,6 +134,7 @@ public class ImcSystem implements Comparable<ImcSystem> {
 		setType(SystemTypeEnum.UNKNOWN);
 		setName(name);
 		setId((id == null)?ImcId16.NULL_ID:id);
+        setCommsInfo(createEmptyCommMean());
 	}
 
 	/**
@@ -141,7 +143,8 @@ public class ImcSystem implements Comparable<ImcSystem> {
 	 */
 	public ImcSystem(ImcId16 id, CommMean commMean) {
 		this(id);
-		setCommsInfo((commMean == null)?null:commMean);
+		if (commMean != null)
+		    setCommsInfo(commMean);
 	}
 
 	/**
@@ -151,10 +154,14 @@ public class ImcSystem implements Comparable<ImcSystem> {
 	 */
 	public ImcSystem(String name, ImcId16 id, CommMean commMean) {
 		this(name, id);
-		setCommsInfo((commMean == null)?null:commMean);
+        if (commMean != null)
+            setCommsInfo(commMean);
 	}
 
-	
+    private CommMean createEmptyCommMean() {
+        return ImcSystem.createCommMean("localhost", 0, 0, getId(), false, false);
+    }
+
 	/**
 	 * @return the name
 	 */
