6d4ccfc5e3359e919b47cabd49be3d171744ffb2
Eduardo Marques <eduardorbmarques@gmail.com>
2017-04-03 16:13:29 +0100

    Basic test code

--- plugins-dev/nvl_runtime/pt/lsts/neptus/plugins/nvl_runtime/NeptusRuntime.java
+++ plugins-dev/nvl_runtime/pt/lsts/neptus/plugins/nvl_runtime/NeptusRuntime.java
@@ -1,10 +1,14 @@
 package pt.lsts.neptus.plugins.nvl_runtime;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.swing.AbstractAction;
+import javax.swing.JButton;
 
 import com.google.common.eventbus.Subscribe;
 
@@ -14,20 +18,29 @@ import pt.lsts.neptus.comm.manager.imc.ImcSystemsHolder;
 import pt.lsts.neptus.console.ConsoleLayout;
 import pt.lsts.neptus.console.events.ConsoleEventVehicleStateChanged;
 import pt.lsts.neptus.console.events.ConsoleEventVehicleStateChanged.STATE;
+import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.nvl.runtime.Availability;
 import pt.lsts.neptus.nvl.runtime.Filter;
 import pt.lsts.neptus.nvl.runtime.NVLRuntime;
 import pt.lsts.neptus.nvl.runtime.NVLVehicle;
 import pt.lsts.neptus.nvl.runtime.TaskExecution;
 import pt.lsts.neptus.nvl.runtime.TaskSpecification;
+import pt.lsts.neptus.nvl.runtime.VehicleRequirements;
+import pt.lsts.neptus.plugins.PluginDescription;
+import pt.lsts.neptus.plugins.Popup;
+import pt.lsts.neptus.plugins.Popup.POSITION;
 import pt.lsts.neptus.renderer2d.InteractionAdapter;
 import pt.lsts.neptus.types.mission.plan.PlanType;
 import pt.lsts.neptus.types.vehicle.VehicleType.SystemTypeEnum;
 
+@PluginDescription(name = "NVL Runtime Feature", author = "Keila Lima")
+@Popup(pos = POSITION.RIGHT, width=200, height=200, accelerator='y')
 @SuppressWarnings("serial")
 public class NeptusRuntime extends InteractionAdapter implements NVLRuntime {
    private  Map<String,NeptusVehicleAdapter> vehicles;
    private  Map<String,NeptusTaskSpecificationAdapter> tasks; //Or List?
    private  List<NeptusTaskExecutionAdapter> runningTasks;
+   private JButton testButton;
 	/**
      * @param console
      */
@@ -46,11 +59,36 @@ public class NeptusRuntime extends InteractionAdapter implements NVLRuntime {
         for(ImcSystem vec: ImcSystemsHolder.lookupAllActiveSystems()){
             if(vec.getType() == SystemTypeEnum.VEHICLE)
                 vehicles.put(vec.getName(),new NeptusVehicleAdapter(vec,STATE.values()[(int) (vec.getLastAnnounceStateReceived())]));
+            System.out.println("V " + vec.getName());
         }
         //initialize existing plans in the console
         for(PlanType plan: getConsole().getMission().getIndividualPlansList().values()){
             tasks.put(plan.getId(),new NeptusTaskSpecificationAdapter(plan));
+            System.out.println("P " + plan.getId());
+
         }
+        test();
+    }
+    private void test() {
+        testButton = new JButton(
+                new AbstractAction(I18n.text("Test!")) {
+
+                    @Override
+                    public void actionPerformed(ActionEvent e) {   
+                        
+                        // Plano IMC
+                        TaskSpecification ts = getTasks( x -> x.getId().equals("nvlPlan")).get(0);
+                        
+                        // Veículos disponíveis
+                        List<NVLVehicle> v = getVehicles(ts.getRequirements().get(0));
+                  
+                        launchTask(ts, v);
+                        
+                       
+                    }
+                });
+        
+        add(testButton);
 
     }
 
