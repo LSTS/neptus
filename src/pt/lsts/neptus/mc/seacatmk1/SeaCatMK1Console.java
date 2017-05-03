5fb44cebe54cc1c422d6d9b22774220075868be0
Paulo Dias <paulo.sousa.dias@gmail.com>
2017-05-03 21:15:21 +0100

    mc/seacatmk1/SeaCatMK1Console: Adding main console loader for SeaCat-MK1.

---
+++ src/pt/lsts/neptus/mc/seacatmk1/SeaCatMK1Console.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright (c) 2004-2017 Universidade do Porto - Faculdade de Engenharia
+ * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * All rights reserved.
+ * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ *
+ * This file is part of Neptus, Command and Control Framework.
+ *
+ * Commercial Licence Usage
+ * Licencees holding valid commercial Neptus licences may use this file
+ * in accordance with the commercial licence agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in a
+ * written agreement between you and Universidade do Porto. For licensing
+ * terms, conditions, and further information contact lsts@fe.up.pt.
+ *
+ * Modified European Union Public Licence - EUPL v.1.1 Usage
+ * Alternatively, this file may be used under the terms of the Modified EUPL,
+ * Version 1.1 only (the "Licence"), appearing in the file LICENSE.md
+ * included in the packaging of this file. You may not use this work
+ * except in compliance with the Licence. Unless required by applicable
+ * law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific
+ * language governing permissions and limitations at
+ * https://github.com/LSTS/neptus/blob/develop/LICENSE.md
+ * and http://ec.europa.eu/idabc/eupl.html.
+ *
+ * For more information please see <http://lsts.fe.up.pt/neptus>.
+ *
+ * Author: pdias
+ * 03/05/2017
+ */
+package pt.lsts.neptus.mc.seacatmk1;
+
+import pt.lsts.neptus.console.ConsoleLayout;
+import pt.lsts.neptus.gui.Loader;
+import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.loader.NeptusMain;
+import pt.lsts.neptus.mc.lauvconsole.LAUVConsole;
+import pt.lsts.neptus.util.GuiUtils;
+import pt.lsts.neptus.util.conf.ConfigFetch;
+import pt.lsts.neptus.util.conf.GeneralPreferences;
+
+/**
+ * @author pdias
+ *
+ */
+public class SeaCatMK1Console extends LAUVConsole {
+
+    private static final long serialVersionUID = 1L;
+
+    static {
+        consoleURL = "conf/consoles/seacat-mk1.ncon";
+        lauvVehicle = "seacat-mk1-01";
+    }
+    
+    public SeaCatMK1Console() {
+    }
+
+    public static ConsoleLayout create(String[] args) {
+        try {
+            return create(SeaCatMK1Console.class, args);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        GuiUtils.setLookAndFeel();
+        ConfigFetch.initialize();
+
+        loader = new Loader("images/neptus_loader_light.png");
+        GeneralPreferences.initialize();
+
+        loader.start();
+        ConfigFetch.setSuperParentFrameForced(loader);
+
+        boolean neptusLookAndFeel = true;
+        NeptusMain.loadPreRequirementsDataExceptConfigFetch(loader, neptusLookAndFeel);
+
+        ConsoleLayout con = create(new String[0]);
+        NeptusMain.wrapMainApplicationWindowWithCloseActionWindowAdapter(con);
+        
+        loader.setText(I18n.text("Application started"));
+        loader.end();
+    }
+}
