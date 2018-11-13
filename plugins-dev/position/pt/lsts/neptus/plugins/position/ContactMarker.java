0880e826b6518e0f30b51e01c94bd4607b48e7be
Paulo Dias <paulo.sousa.dias@gmail.com>
2018-11-13 20:39:41 +0000

    plugins/position/ContactMarker: Cleanup imports.

--- plugins-dev/position/pt/lsts/neptus/plugins/position/ContactMarker.java
+++ plugins-dev/position/pt/lsts/neptus/plugins/position/ContactMarker.java
@@ -43,7 +43,6 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Date;
 import java.util.LinkedHashMap;
 import java.util.Vector;
