680d42e39d16a65ce795b0edaa0c1914016ee912
kmolima <keilalima.cv@gmail.com>
2019-02-21 15:07:26 +0000

    mra/plots: Reverted to 4da4c5ff2309355df58cd031f885de02483901da

--- src/pt/lsts/neptus/mra/plots/ScriptedPlot.java
+++ src/pt/lsts/neptus/mra/plots/ScriptedPlot.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2004-2019 Universidade do Porto - Faculdade de Engenharia
+ * Copyright (c) 2004-2018 Universidade do Porto - Faculdade de Engenharia
  * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
  * All rights reserved.
  * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
@@ -15,7 +15,7 @@
  *
  * Modified European Union Public Licence - EUPL v.1.1 Usage
  * Alternatively, this file may be used under the terms of the Modified EUPL,
- * Version 1.1 only (the "Licence"), appearing in the file LICENSE.md
+ * Version 1.1 only (the "Licence"), appearing in the file LICENCE.md
  * included in the packaging of this file. You may not use this work
  * except in compliance with the Licence. Unless required by applicable
  * law or agreed to in writing, software distributed under the Licence is
@@ -27,225 +27,289 @@
  *
  * For more information please see <http://lsts.fe.up.pt/neptus>.
  *
- * Author: keila
- * Feb 8, 2019
+ * Author: José Pinto
+ * Nov 28, 2012
  */
 package pt.lsts.neptus.mra.plots;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import org.codehaus.groovy.control.CompilerConfiguration;
-import org.codehaus.groovy.control.customizers.ImportCustomizer;
-import org.jfree.data.time.TimeSeries;
-import org.jfree.data.time.TimeSeriesDataItem;
+import org.mozilla.javascript.Context;
+import org.mozilla.javascript.Script;
+import org.mozilla.javascript.ScriptableObject;
+import org.mozilla.javascript.tools.shell.Global;
 
-import groovy.lang.GroovyShell;
-import pt.lsts.imc.IMCMessage;
 import pt.lsts.imc.lsf.LsfIndex;
+import pt.lsts.neptus.NeptusLog;
+import pt.lsts.neptus.i18n.I18n;
 import pt.lsts.neptus.mra.LogMarker;
 import pt.lsts.neptus.mra.MRAPanel;
 import pt.lsts.neptus.util.GuiUtils;
 
 /**
- * @author keila
- *
+ * @author zp
+ * 
  */
 public class ScriptedPlot extends MRATimeSeriesPlot {
 
-    private GroovyShell shell;
-    private LsfIndex index;
-    private final String scriptPath;
-    private Map<String, TimeSeries> values = new LinkedHashMap<>();
-    private Map<String, String> seriesIndex = new HashMap<>();
-    private MRAPanel mra;
-    private String title = null;
+    protected LinkedHashMap<String, String> traces = new LinkedHashMap<>();
+    protected LinkedHashMap<String, String> hiddenTraces = new LinkedHashMap<>();
+    protected LinkedHashMap<String, Script> scripts = new LinkedHashMap<>();
 
-    /**
-     * @param panel
-     */
-    public ScriptedPlot(MRAPanel panel, String path) {
-        super(panel);
-        mra = panel;
-        scriptPath = path;
-        index = mra.getSource().getLsfIndex();
-        String[] fields = new String[1];
-        fields[0] = "EstimatedState.depth";
+    protected String init = null, end = null;
+    protected Script initScript = null, endScript = null;
 
-        // init shell
-        CompilerConfiguration cnfg = new CompilerConfiguration();
-        ImportCustomizer imports = new ImportCustomizer();
-        imports.addStarImports("pt.lsts.imc", "java.lang.Math", "pt.lsts.neptus.mra.plots");
-        imports.addStaticStars("pt.lsts.neptus.mra.plots.ScriptedPlotGroovy");
-        cnfg.addCompilationCustomizers(imports);
-        shell = new GroovyShell(this.getClass().getClassLoader(), cnfg);
-        runScript(scriptPath);
-    }
+    protected ScriptableIndex scIndex = null;
+    protected Context context;
+    protected Global global;
+    protected LsfIndex index;
+    protected String title = getClass().getName();
+    protected ScriptEnvironment env = new ScriptEnvironment();
 
-    public String[] msgField(String expr) {
-        String msg, field, entity;
-        String[] fields = expr.split(".");
-        if (fields.length == 2) {
-            msg = fields[0];
-            field = fields[1];
-            entity = null;
-        }
-        else if (fields.length == 3) {
-            msg = fields[0];
-            entity = fields[1];
-            field = fields[2];
-        }
-        return fields;
-    }
 
-    public void title(String t) {
-        title = t;
-    }
-
-    public List<TimeSeries> getDataFromExpr(String expr) {
-        List<TimeSeries> result = new ArrayList<>();
-
-        if (tsc.getSeries(expr) == null) {
-            GenericPlot p = new GenericPlot(msgField(expr), mra);// TODO Optimize
-            p.process(index);
-            for (TimeSeries t : p.series.values()) {
-                result.add(t);
-                tsc.addSeries(t);
-            }
-        }
-
-        return result;
-    }
-    
     @Override
     public String getName() {
-        if(title == null)
-            return  Arrays.toString(seriesIndex.keySet().toArray());
-        else 
-            return title;
+        return I18n.text(title);
     }
 
-    /**
-     * Runs the Groovy script after verifying its validity by parsing it.
-     * 
-     * @param script Text script
-     */
-    public void runScript(String path) {
-        StringBuilder sb = new StringBuilder();
+    @Override
+    public String getTitle() {
+        return I18n.textf("%plotname plot", title);
+    }
+    public ScriptedPlot(MRAPanel panel, String scriptFile) {
+        super(panel);
+
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(path));
-            int c;
-            while ((c = reader.read()) != -1) {
-                sb.append((char) c);
+            BufferedReader reader = new BufferedReader(new FileReader(scriptFile));
+            title = reader.readLine();
+            String line;
+            while((line = reader.readLine()) != null) {
+                if (line.trim().isEmpty() || line.trim().startsWith("#"))
+                    continue;
+
+                while (line.endsWith("\\")) {
+                    line = line.substring(0, line.length()-1) + reader.readLine();
+                }
+
+                String parts[] = line.trim().split(":");
+
+                String script = parts[1];
+
+                script = script.replaceAll("\\$\\{([^\\}]*)\\}", "log.val(\"$1\")");
+                script = script.replaceAll("mark\\(([^\\)]+)\\)", "log.mark($1)");
+                script = script.replaceAll("\\$time", "log.time()");
+                script = script.replaceAll("\\$(\\w+)", "env[\"$1\"]");
+
+                if (parts[0].isEmpty())
+                    hiddenTraces.put(parts[1], script);
+                else if (parts[0].equals("init"))
+                    init = script;
+                else if (parts[0].equals("end"))
+                    end = script;
+                else
+                    traces.put(parts[0], script);                
             }
-            shell.setVariable("plot", ScriptedPlot.this);
-            String defplot = "configPlot plot";
-            boolean b = shell.getVariable("plot") == null;
-            System.err.println("plot var equals null? "+b);
-            shell.evaluate(defplot); 
-            //shell.invokeMethod("configPlot", this);
-            String script = sb.toString();
-            shell.parse(script);
-            shell.evaluate(script);
+
             reader.close();
         }
         catch (Exception e) {
-            GuiUtils.errorMessage(mra, "Error Parsing Script", e.getLocalizedMessage());
+            GuiUtils.errorMessage(panel, e);
+        }       
+    }
+
+    protected void init() {
+
+        context = Context.enter();
+        context.initStandardObjects();
+        global = new Global(context);
+        Object o = Context.javaToJS(scIndex, global);
+        ScriptableObject.putProperty(global, "log", o);
+        ScriptableObject.putProperty(global, "env", env);
+        ScriptableObject.putProperty(global, "mraPanel", mraPanel);
+
+        if (init != null) {
+            try {
+                initScript = context.compileString(init, "init", 1, null);
+            }
+            catch (Exception e) {
+                GuiUtils.errorMessage(mraPanel, "Init script Error", e.getMessage());
+                e.printStackTrace();
+            }
+            NeptusLog.pub().debug("init");
+        }
+
+        if (end != null) {
+            try {
+                endScript = context.compileString(end, "end", 1, null);
+            }
+            catch (Exception e) {
+                GuiUtils.errorMessage(mraPanel, "End script Error", e.getMessage());
+                e.printStackTrace();
+            }
+            NeptusLog.pub().debug("ended.");
+        }
+
+        for (Entry<String, String> t : traces.entrySet()) {
+            String script = t.getValue();
+            try {
+                context.evaluateString(global, script, t.getKey(), 1, null);
+                scripts.put(t.getKey(), context.compileString(script, t.getKey(), 1, null));
+            }
+            catch (Exception e) {
+                GuiUtils.errorMessage(mraPanel, "Plot script Error", e.getMessage());
+                e.printStackTrace();
+            }
+        }
+
+        for (Entry<String, String> t : hiddenTraces.entrySet()) {
+            String trace = t.getValue();
+            try {
+                context.evaluateString(global, trace, t.getKey(), 1, null);
+                scripts.put(t.getKey(), context.compileString(trace, t.getKey(), 1, null));
+            }
+            catch (Exception e) {
+                GuiUtils.errorMessage(mraPanel, "Script Error", e.getMessage());
+                e.printStackTrace();
+            }
         }
     }
 
-    /**
-     * Adds a new time series to the existing plot. If the series already exists, it updates it.
-     * 
-     * @param ts the TimeSeries to be added
-     */
-    public void addTimeSeries(TimeSeries ts) {
-        String trace = ts.getKey().toString();
-
-        if (forbiddenSeries.contains(trace))
-            return;
-
-        if (!super.series.containsKey(trace)) {
-            addTrace(trace);
-        }
-        for (int i = 0; i < ts.getItemCount(); i++) {
-            TimeSeriesDataItem value = ts.getDataItem(i);
-            super.series.get(trace).addOrUpdate(value);
-        }
-    }
-    
-    public void addTimeSeries(String id, String query) {
-        seriesIndex.put(id, query);
-    }
-
-    public void mark(double time, String label) {
-        mra.addMarker(new LogMarker(label, time * 1000, 0, 0));
-    }
-
-    public void addQuery(String id, String field) {
-        seriesIndex.put(id, field);
-    }
-
-    /*
-     * (non-Javadoc)
-     * 
-     * @see pt.lsts.neptus.mra.plots.MRATimeSeriesPlot#canBeApplied(pt.lsts.imc.lsf.LsfIndex)
-     */
     @Override
     public boolean canBeApplied(LsfIndex index) {
-        for (Entry<String, String> entry : seriesIndex.entrySet()) {
-            String field = entry.getValue();
-            String messageName = field.split("\\.")[0];
-            if (!index.containsMessagesOfType(messageName))
-                return false;
-        }
+        this.index = index;
         return true;
     }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see pt.lsts.neptus.mra.plots.MRATimeSeriesPlot#process(pt.lsts.imc.lsf.LsfIndex) adapted from @GenericPlot
-     */
     @Override
     public void process(LsfIndex source) {
-        for (Entry<String, String> entry : seriesIndex.entrySet()) {
-            String field, entity;
-            String id = entry.getKey();
-            field = entry.getValue();
-            String messageName = field.split("\\.")[0];
-            String variable = field.split("\\.")[1];
-            System.err.println("Variable " + variable);
-            for (IMCMessage m : source.getIterator(messageName, 0, (long) (timestep * 1000))) {
+        this.scIndex = new ScriptableIndex(source, 0);
+        this.env = new ScriptEnvironment();
+        this.index = source;
+        init();
 
-                String seriesName = "";
-                if (id.equals(field)) {
-                    if (m.getValue("id") != null) {
-                        seriesName = m.getSourceName() + "." + source.getEntityName(m.getSrc(), m.getSrcEnt()) + "."
-                                + field + "." + m.getValue("id");
+        if (initScript != null)
+            initScript.exec(context, global);        
+
+        double step = Math.max(timestep, 0.05);
+        for (int i = index.advanceToTime(0, step); i != -1; 
+                i = index.advanceToTime(i, index.timeOf(i) + step)) {
+
+            scIndex.lsfPos = i;
+            for (String trace : scripts.keySet()) {
+                if (traces.containsKey(trace)) {
+                    Object ret = scripts.get(trace).exec(context, global);
+                    if (ret != null && ret instanceof Number) {
+                        double val = ((Number)ret).doubleValue();
+                        addValue((long)(index.timeOf(i)*1000), trace, val);
+                    }
+                }
+                else {
+                    scripts.get(trace).exec(context, global);
+                }
+            }            
+        }
+
+        if (endScript != null)
+            endScript.exec(context, global);
+
+    }
+
+    /**
+     * This internal class allows plot scripts to access the current log time and message fields in the log
+     * 
+     * @author zp
+     */
+    public class ScriptableIndex {
+
+        protected LsfIndex lsfIndex;
+        protected int lsfPos;
+        protected int prevPos = 0;
+
+        /**
+         * Class constructor is passed the LsfIndex and an initial index (usually 0)
+         * 
+         * @param source The index to be used by scripts
+         * @param curIndex The position in the index
+         */
+        public ScriptableIndex(LsfIndex source, int curIndex) {
+            this.lsfIndex = source;
+            this.lsfPos = curIndex;            
+        }
+
+        public void mark(double time, String label) {
+            mraPanel.addMarker(new LogMarker(label, time * 1000,0,0));
+        }
+
+        public void mark(String label) {
+            mark(lsfIndex.timeOf(lsfPos), label);
+        }
+
+        /**
+         * This method returns the current log time
+         * 
+         * @return current log time
+         */
+        public double time() {
+            return lsfIndex.timeOf(lsfPos);
+        }
+
+        /**
+         * This method evaluates a field expression (like "EstimatedState[Navigation].x") and returns its current value
+         * in the log
+         * 
+         * @param expression The expression to be valuated
+         * @return The value (double) or Double.NaN if the expression is invalid or the log does not contain the
+         *         required fields at current time
+         */
+        public double val(String expression) {
+
+            Pattern p = Pattern.compile("(\\w+)(\\[(\\w+)\\])?\\.(\\w+)");
+            Matcher m = p.matcher(expression);
+
+            if (!m.matches()) {
+                return Double.NaN;
+            }
+            String message, entity, field;
+            message = m.group(1);
+            if (m.groupCount() > 2) {
+                entity = m.group(3);
+                field = m.group(4);
+            }
+            else {
+                entity = null;
+                field = m.group(2);
+            }
+
+            int msgType = index.getDefinitions().getMessageId(message);
+
+            if (entity == null) {
+                int msgIdx = index.getPreviousMessageOfType(msgType, lsfPos);
+                if (msgIdx == -1)
+                    return Double.NaN;
+                else
+                    return index.getMessage(msgIdx).getDouble(field);
+            }
+            else {
+                int msgIdx = index.getPreviousMessageOfType(msgType, lsfPos);
+                while (msgIdx >= prevPos) {
+
+                    if (msgIdx == -1)
+                        return Double.NaN;
+                    else if (index.entityNameOf(msgIdx).equals(entity)) {
+                        prevPos = msgIdx;
+                        return index.getMessage(msgIdx).getDouble(field);
                     }
                     else {
-                        seriesName = m.getSourceName() + "." + source.getEntityName(m.getSrc(), m.getSrcEnt()) + "."
-                                + field;
+                        msgIdx = index.getPreviousMessageOfType(msgType, msgIdx);
                     }
                 }
-                else
-                    seriesName = m.getSourceName() + "." + id;
-                if (m.getMessageType().getFieldUnits(variable) != null
-                        && m.getMessageType().getFieldUnits(variable).startsWith("rad")) {
-                    // Special case for angles in radians
-                    addValue(m.getTimestampMillis(), seriesName, Math.toDegrees(m.getDouble(variable)));
-                }
-                else
-                    addValue(m.getTimestampMillis(), seriesName, m.getDouble(variable));
             }
+            return Double.NaN;
         }
     }
 }
