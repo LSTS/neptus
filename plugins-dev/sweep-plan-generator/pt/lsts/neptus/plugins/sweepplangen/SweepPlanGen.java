6cba7c07e0c0ea2649f41c3798301a7e85f46c82
edramos-97 <up201505779@fe.up.pt>
2019-02-26 19:33:25 +0000

    plugins/SweepPlanGen: Improved import statements and added copyright statement

--- plugins-dev/sweep-plan-generator/pt/lsts/neptus/plugins/sweepplangen/SweepPlanGen.java
+++ plugins-dev/sweep-plan-generator/pt/lsts/neptus/plugins/sweepplangen/SweepPlanGen.java
@@ -1,11 +1,74 @@
-package pt.lsts.neptus.plugins;
+/*
+ * Copyright (c) 2004-2018 Universidade do Porto - Faculdade de Engenharia
+ * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * All rights reserved.
+ * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ *
+ * This file is part of Neptus, Command and Control Framework.
+ *
+ * Commercial Licence Usage
+ * Licencees holding valid commercial Neptus licences may use this file
+ * in accordance with the commercial licence agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in a
+ * written agreement between you and Universidade do Porto. For licensing
+ * terms, conditions, and further information contact lsts@fe.up.pt.
+ *
+ * Modified European Union Public Licence - EUPL v.1.1 Usage
+ * Alternatively, this file may be used under the terms of the Modified EUPL,
+ * Version 1.1 only (the "Licence"), appearing in the file LICENSE.md
+ * included in the packaging of this file. You may not use this work
+ * except in compliance with the Licence. Unless required by applicable
+ * law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific
+ * language governing permissions and limitations at
+ * https://github.com/LSTS/neptus/blob/develop/LICENSE.md
+ * and http://ec.europa.eu/idabc/eupl.html.
+ *
+ * For more information please see <http://lsts.fe.up.pt/neptus>.
+ *
+ * Author: Eduardo Ramos
+ * 28 January 2019
+ */
+package pt.lsts.neptus.plugins.sweepplangen;
 
 import com.l2fprod.common.propertysheet.DefaultProperty;
 import com.l2fprod.common.propertysheet.Property;
 import com.l2fprod.common.propertysheet.PropertySheetPanel;
 import com.l2fprod.common.swing.renderer.DefaultCellRenderer;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Graphics2D;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Vector;
+import javax.swing.JComboBox;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPopupMenu;
+import javax.swing.JSpinner;
+import javax.swing.JSplitPane;
+import javax.swing.SpinnerNumberModel;
+import javax.swing.SwingUtilities;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.event.ItemEvent;
+import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Point2D;
+import java.beans.PropertyChangeEvent;
 import pt.lsts.neptus.console.ConsoleLayout;
-import pt.lsts.neptus.console.plugins.MainVehicleChangeListener;
 import pt.lsts.neptus.console.plugins.planning.edit.AllManeuversPayloadSettingsChanged;
 import pt.lsts.neptus.data.Pair;
 import pt.lsts.neptus.gui.PropertiesEditor;
@@ -15,11 +78,19 @@ import pt.lsts.neptus.gui.editor.renderer.I18nCellRenderer;
 import pt.lsts.neptus.i18n.I18n;
 import pt.lsts.neptus.mp.ManeuverLocation;
 import pt.lsts.neptus.mp.actions.PlanActions;
-import pt.lsts.neptus.mp.maneuvers.*;
+import pt.lsts.neptus.mp.maneuvers.FollowPath;
+import pt.lsts.neptus.mp.maneuvers.FollowTrajectory;
+import pt.lsts.neptus.mp.maneuvers.Goto;
+import pt.lsts.neptus.mp.maneuvers.PopUp;
+import pt.lsts.neptus.mp.maneuvers.ScheduledGoto;
+import pt.lsts.neptus.mp.maneuvers.StationKeeping;
 import pt.lsts.neptus.params.ConfigurationManager;
 import pt.lsts.neptus.params.ManeuverPayloadConfig;
 import pt.lsts.neptus.params.SystemProperty;
 import pt.lsts.neptus.params.renderer.I18nSystemPropertyRenderer;
+import pt.lsts.neptus.plugins.NeptusProperty;
+import pt.lsts.neptus.plugins.PluginDescription;
+import pt.lsts.neptus.plugins.PluginUtils;
 import pt.lsts.neptus.renderer2d.InteractionAdapter;
 import pt.lsts.neptus.renderer2d.Renderer2DPainter;
 import pt.lsts.neptus.renderer2d.StateRenderer2D;
@@ -31,23 +102,11 @@ import pt.lsts.neptus.types.mission.plan.PlanType;
 import pt.lsts.neptus.types.vehicle.VehiclesHolder;
 import pt.lsts.neptus.util.GuiUtils;
 
-import javax.swing.*;
-import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
-import javax.swing.border.TitledBorder;
-import javax.xml.stream.Location;
-import java.awt.*;
-import java.awt.event.*;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.Point2D;
-import java.beans.PropertyChangeEvent;
-import java.util.*;
-import java.util.List;
 
 @PluginDescription(name = "Sweep Plan Generator")
 public class SweepPlanGen extends InteractionAdapter implements Renderer2DPainter {
 
-    private JPanel sidePanel = null;
+    private JPanel sidePanel;
     private PropertySheetPanel propsPanel = null;
     private PropertiesTable propTable = null;
     private HashMap<Component, Object> componentList = new HashMap<>();
@@ -70,6 +129,7 @@ public class SweepPlanGen extends InteractionAdapter implements Renderer2DPainte
 
     public SweepPlanGen(ConsoleLayout console) {
         super(console);
+        sidePanel = null;
     }
 
     @Override
@@ -469,10 +529,10 @@ public class SweepPlanGen extends InteractionAdapter implements Renderer2DPainte
         JPopupMenu popup = new JPopupMenu();
 
         if (startPoint != null && isVertex(source, lt, startPoint)) {
-           popup.add("<html><b>Remove</b> start point").addActionListener(e1 -> {
-               startPoint = null;
-               updatePlan(source);
-           });
+            popup.add("<html><b>Remove</b> start point").addActionListener(e1 -> {
+                startPoint = null;
+                updatePlan(source);
+            });
         }
 
         if (endPoint != null && isVertex(source, lt, endPoint)) {
