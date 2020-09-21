6bb2152ce642eeb75970411cd1a55d72d368cba3
Jose Pinto <zepinto@gmail.com>
2020-09-21 22:02:51 +0100

    xyztiles.sh: Execute exporter recursively.

--- helper-scripts/xyztiles.sh
+++ helper-scripts/xyztiles.sh
@@ -2,5 +2,12 @@
 
 classpath=`find .. -name *.jar | tr '\n' ':'`
 
-java -cp $classpath -Xms20m -Xmx4096m pt.lsts.neptus.plugins.bathym.BatchXyzExporter $@
+for f in `find $@ -name *.83P`; do 
+  folder=$(dirname $f); 
+  echo "processing $folder.";
+  java -cp $classpath -Xms20m -Xmx4096m pt.lsts.neptus.plugins.bathym.BatchXyzExporter $folder;
+done
+
+
+#java -cp $classpath -Xms20m -Xmx4096m pt.lsts.neptus.plugins.bathym.BatchXyzExporter $@
 
