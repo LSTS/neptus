7c354e0c17685a0e2bb878a9e2c70041aefff5e9
Paulo Dias <paulo.sousa.dias@gmail.com>
2021-03-01 20:43:17 +0000

    plugins/envdisp/painter/EnvDataPaintHelper: Cleanup imports.

--- plugins-dev/envdisp/src/java/pt/lsts/neptus/plugins/envdisp/painter/EnvDataPaintHelper.java
+++ plugins-dev/envdisp/src/java/pt/lsts/neptus/plugins/envdisp/painter/EnvDataPaintHelper.java
@@ -55,7 +55,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.DoubleAccumulator;
 import java.util.concurrent.atomic.LongAccumulator;
 import java.util.function.BiConsumer;
