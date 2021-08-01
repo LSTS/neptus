f3934027780f64c8abbfdbe77c64f6edcb6ece63
Paulo Dias <paulo.sousa.dias@gmail.com>
2021-08-01 21:17:19 +0100

    plugins/textCommands/TextCommands: Tweaks on text commands with adding dislodge and generic text commands, and gui tweaks.

---
+++ plugins-dev/textCommands/src/java/pt/lsts/neptus/plugins/txtcmd/CommandText.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2004-2021 Universidade do Porto - Faculdade de Engenharia
+ * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * All rights reserved.
+ * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ *
+ * This file is part of Neptus, Command and Control Framework.
+ *
+ * Commercial Licence Usage
+ * Licencees holding valid commercial Neptus licences may use this file
+ * in accordance with the commercial licence agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in a
+ * written agreement between you and Universidade do Porto. For licensing
+ * terms, conditions, and further information contact lsts@fe.up.pt.
+ *
+ * Modified European Union Public Licence - EUPL v.1.1 Usage
+ * Alternatively, this file may be used under the terms of the Modified EUPL,
+ * Version 1.1 only (the "Licence"), appearing in the file LICENSE.md
+ * included in the packaging of this file. You may not use this work
+ * except in compliance with the Licence. Unless required by applicable
+ * law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific
+ * language governing permissions and limitations at
+ * https://github.com/LSTS/neptus/blob/develop/LICENSE.md
+ * and http://ec.europa.eu/idabc/eupl.html.
+ *
+ * For more information please see <http://lsts.fe.up.pt/neptus>.
+ *
+ * Author: pdias
+ * 1 Ago, 2021
+ */
+package pt.lsts.neptus.plugins.txtcmd;
+
+
+import pt.lsts.neptus.plugins.NeptusProperty;
+
+/**
+ * @author pdias
+ *
+ */
+public class CommandText extends AbstractTextCommand {
+
+    @NeptusProperty(description = "Use 'TXT' or none or any other prefix to match the protocol")
+    String prefix = "TXT";
+
+    @NeptusProperty
+    String command = "";
+
+    @Override
+    public String getCommand() {
+        return "generic text";
+    }
+
+    @Override
+    public String buildCommand() {
+        String cmd = super.buildCommand();
+        cmd = cmd.replaceFirst("^" + getCommand(), "").trim();
+        cmd = cmd.replaceFirst("^prefix=(.*)?;", "$1").trim();
+        cmd = cmd.replaceFirst("command=(.*)?", " $1").trim();
+        return cmd;
+    }
+}
