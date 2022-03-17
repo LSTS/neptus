07fb672ae126e907b034cbb736a4631a4a3f0d88
Paulo Dias <paulo.sousa.dias@gmail.com>
2022-03-17 11:16:20 +0000

    mp/maneuvers/AreaSurvey: Fix param editor to show angle as degrees.

--- src/java/pt/lsts/neptus/mp/maneuvers/AreaSurvey.java
+++ src/java/pt/lsts/neptus/mp/maneuvers/AreaSurvey.java
@@ -62,6 +62,8 @@ import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.console.ConsoleInteraction;
 import pt.lsts.neptus.gui.LocationPanel;
 import pt.lsts.neptus.gui.editor.AngleEditorRads;
+import pt.lsts.neptus.gui.editor.AngleEditorRadsShowDegrees;
+import pt.lsts.neptus.gui.editor.renderer.AngleRadsRenderer;
 import pt.lsts.neptus.i18n.I18n;
 import pt.lsts.neptus.mp.ManeuverLocation;
 import pt.lsts.neptus.plugins.NeptusProperty;
@@ -94,7 +96,8 @@ public class AreaSurvey extends FollowPath {
     @NeptusProperty(name = "Horizontal Step", description="Distance, in meters between transects", units = "m")
     protected double horizontalStep = 30;
 
-    @NeptusProperty(name = "Survey Bearing", description="Angle to use for the survey transects", editorClass = AngleEditorRads.class)
+    @NeptusProperty(name = "Survey Bearing", description="Angle to use for the survey transects",
+            editorClass = AngleEditorRadsShowDegrees.class, rendererClass = AngleRadsRenderer.class)
     protected double angle = 0;
 
     @NeptusProperty(name = "Automatic Bearing", description="Calculate the survey bearing angle automatically (minimizing number of turns)")
