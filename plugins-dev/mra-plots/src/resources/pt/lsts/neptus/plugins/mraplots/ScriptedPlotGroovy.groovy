fd40f8bb2a3f1c183adf271dd5fc432b7dcdf7e8
Paulo Dias <paulo.sousa.dias@gmail.com>
2023-02-05 14:52:26 +0000

    plugins/ScriptedPlotGroovy.groovy: Added angle closures to transform to and from degrees and radians.

--- plugins-dev/mra-plots/src/resources/pt/lsts/neptus/plugins/mraplots/ScriptedPlotGroovy.groovy
+++ plugins-dev/mra-plots/src/resources/pt/lsts/neptus/plugins/mraplots/ScriptedPlotGroovy.groovy
@@ -341,6 +341,10 @@ class ScriptedPlotGroovy  {
             Double.NaN             
             }
 
+    static public toDegrees = {double rad -> Math.toDegrees(rad)}
+
+    static public toRadians = {double deg -> Math.toRadians(deg)}
+
     static public normalizeAngleRads2Pi = {double double1 -> AngleUtils.nomalizeAngleRads2Pi(double1)}
 
     static public normalizeAngleRadsPi = {double double1 -> AngleUtils.nomalizeAngleRadsPi(double1)}
