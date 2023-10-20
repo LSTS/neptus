bc4e78e877cb09bcf414fbd65767ff8d427c03df
Pedro Costa <pedro.costa@lsts.pt>
2023-10-20 17:05:36 +0100

    mra/importers/sdf/SdfParser: Remove unused field.

--- src/java/pt/lsts/neptus/mra/importers/sdf/SdfParser.java
+++ src/java/pt/lsts/neptus/mra/importers/sdf/SdfParser.java
@@ -73,7 +73,6 @@ public class SdfParser {
 
     final static int SUBSYS_LOW = 3501;
     final static int SUBSYS_HIGH = 3502;
-    final static int BATHY_PULSE_COMPRESSED = 3503;
 
     public SdfParser(File[] files) {
         Arrays.sort(files);
