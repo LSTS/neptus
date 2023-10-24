6086c95422673155012f12dfe1f0d2491b3c81f4
Pedro Costa <pedro.costa@lsts.pt>
2023-10-24 18:15:08 +0100

    mra/importers/sdf/SdfParser: Add back BATHY_PULSE_COMPRESSED.

--- src/java/pt/lsts/neptus/mra/importers/sdf/SdfParser.java
+++ src/java/pt/lsts/neptus/mra/importers/sdf/SdfParser.java
@@ -72,6 +72,7 @@ public class SdfParser {
 
     final static int SUBSYS_LOW = 3501;
     final static int SUBSYS_HIGH = 3502;
+    final static int BATHY_PULSE_COMPRESSED = 3503;     // Will be used in the future
 
     public SdfParser(File[] files) {
 
