1c4577a8ffdbc4c744ae991921a74a33061d5542
Pedro Costa <pedro.costa@lsts.pt>
2023-10-25 16:02:26 +0100

    mra/importers/sdf/SdfConstant: Rename class from SdfConstants to SdfConstant.

--- src/java/pt/lsts/neptus/mra/importers/sdf/SdfConstant.java
+++ src/java/pt/lsts/neptus/mra/importers/sdf/SdfConstant.java
@@ -1,6 +1,6 @@
 package pt.lsts.neptus.mra.importers.sdf;
 
-public class SdfConstants {
+public class SdfConstant {
     final static int SUBSYS_LOW = 3501;
     final static int SUBSYS_HIGH = 3502;
     final static int BATHY_PULSE_COMPRESSED = 3503;     // Will be used in the future
