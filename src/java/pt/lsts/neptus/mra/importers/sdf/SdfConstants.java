7c0f6216c0529d445b94adfb35a3f8f2a8476269
Pedro Costa <pedro.costa@lsts.pt>
2023-10-25 15:59:57 +0100

    mra/importers/sdf/SdfConstants: Create class.

---
+++ src/java/pt/lsts/neptus/mra/importers/sdf/SdfConstants.java
@@ -0,0 +1,7 @@
+package pt.lsts.neptus.mra.importers.sdf;
+
+public class SdfConstants {
+    final static int SUBSYS_LOW = 3501;
+    final static int SUBSYS_HIGH = 3502;
+    final static int BATHY_PULSE_COMPRESSED = 3503;     // Will be used in the future
+}
