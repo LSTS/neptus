908594dc1b625fa0e1458240b9128a1c89af6250
Pedro Costa <pedro.costa@lsts.pt>
2023-10-26 15:41:00 +0100

    mra/importers/sdf/SdfIndex.java: Remove imports.

--- src/java/pt/lsts/neptus/mra/importers/sdf/SdfIndex.java
+++ src/java/pt/lsts/neptus/mra/importers/sdf/SdfIndex.java
@@ -33,9 +33,7 @@
 package pt.lsts.neptus.mra.importers.sdf;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
