f5f6d53f4c5ad6e54a2a93399daa44c28b2f8caf
Pedro Costa <pedro.costa@lsts.pt>
2023-11-03 10:43:16 +0000

    plugins/deepvision/DvsIndex: Add save method.

--- plugins-dev/deepvision-sidescan/src/java/pt/lsts/neptus/plugins/deepvision/DvsIndex.java
+++ plugins-dev/deepvision-sidescan/src/java/pt/lsts/neptus/plugins/deepvision/DvsIndex.java
@@ -1,15 +1,30 @@
 package pt.lsts.neptus.plugins.deepvision;
 
-import java.io.FileInputStream;
-import java.io.ObjectOutputStream;
-import java.util.ArrayList;
+import pt.lsts.neptus.NeptusLog;
 
-public class DvsIndex {
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.io.FileOutputStream;
+
+public class DvsIndex implements Serializable {
+    private static final long serialVersionUID = 1L;
     private DvsHeader dvsHeader;
     private ArrayList<Long> timestamps;
 
     public DvsIndex(DvsHeader dvsHeader, ArrayList<Long> timestamps) {
         this.dvsHeader = dvsHeader;
         this.timestamps = timestamps;
+    }
+
+    public void save(String filePath) {
+        try (ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(filePath))){
+            out.writeObject(this);
+        }
+        catch (IOException e) {
+            NeptusLog.pub().error("Could not save index to " + filePath);
+            e.printStackTrace();
+        }
     }
 }
