a9391cbe1315a8a0ae343e30b1b2cbefa1b5bd8c
Pedro Costa <pedro.costa@lsts.pt>
2023-11-06 13:19:45 +0000

    plugins/deepvision/DvsPing: Make fields final.

--- plugins-dev/deepvision-sidescan/src/java/pt/lsts/neptus/plugins/deepvision/DvsPing.java
+++ plugins-dev/deepvision-sidescan/src/java/pt/lsts/neptus/plugins/deepvision/DvsPing.java
@@ -33,8 +33,8 @@
 package pt.lsts.neptus.plugins.deepvision;
 
 public class DvsPing {
-    private DvsPos dvsPos;
-    private DvsReturn dvsReturn;
+    private final DvsPos dvsPos;
+    private final DvsReturn dvsReturn;
 
     public DvsPing(DvsPos dvsPos, DvsReturn dvsReturn) {
         this.dvsPos = dvsPos;
