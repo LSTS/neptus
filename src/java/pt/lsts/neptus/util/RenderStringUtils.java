62bbb2ff1758eac40837441bce6470944ccbd999
Miguel Carvalho <miguel.paz.carvalho@outlook.pt>
2024-11-11 16:39:12 +0000

    util/RenderStringUtils: Created util for string outline rendering.

---
+++ src/java/pt/lsts/neptus/util/RenderStringUtils.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) 2004-2024 Universidade do Porto - Faculdade de Engenharia
+ * Laboratório de Sistemas e Tecnologia Subaquática (LSTS)
+ * All rights reserved.
+ * Rua Dr. Roberto Frias s/n, sala I203, 4200-465 Porto, Portugal
+ *
+ * This file is part of Neptus, Command and Control Framework.
+ *
+ * Commercial Licence Usage
+ * Licencees holding valid commercial Neptus licences may use this file
+ * in accordance with the commercial licence agreement provided with the
+ * Software or, alternatively, in accordance with the terms contained in a
+ * written agreement between you and Universidade do Porto. For licensing
+ * terms, conditions, and further information contact lsts@fe.up.pt.
+ *
+ * Modified European Union Public Licence - EUPL v.1.1 Usage
+ * Alternatively, this file may be used under the terms of the Modified EUPL,
+ * Version 1.1 only (the "Licence"), appearing in the file LICENCE.md
+ * included in the packaging of this file. You may not use this work
+ * except in compliance with the Licence. Unless required by applicable
+ * law or agreed to in writing, software distributed under the Licence is
+ * distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the Licence for the specific
+ * language governing permissions and limitations at
+ * https://github.com/LSTS/neptus/blob/develop/LICENSE.md
+ * and http://ec.europa.eu/idabc/eupl.html.
+ *
+ * For more information please see <http://lsts.fe.up.pt/neptus>.
+ *
+ * Author: Miguel Carvalho
+ * 2024/11/04
+ */
+package pt.lsts.neptus.util;
+
+import java.awt.*;
+import java.awt.font.GlyphVector;
+
+public class RenderStringUtils {
+
+    /** To avoid instantiation */
+    private RenderStringUtils() {
+    }
+
+    /**
+     * Renders an outline of string text.
+     *
+     * @param g
+     * @param font
+     * @param textColor
+     * @param outlineColor
+     * @param text
+     * @param x
+     * @param y
+     * @return
+     */
+    public static void drawStringWOutline(Graphics2D g, Font font, Color textColor, Color outlineColor, String text, double x, double y) {
+        g.setFont(font);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+        // Calculate dynamic outline thickness based on font size
+        float outlineThickness = font.getSize() * 0.25f;
+
+        GlyphVector glyphVector = font.createGlyphVector(g.getFontRenderContext(), text);
+        Shape textShape = glyphVector.getOutline((int) (x), (int) (y));
+
+        // Draw the outline
+        g.setColor(outlineColor);
+        g.setStroke(new BasicStroke(outlineThickness, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); // Thickness of the outline
+        g.draw(textShape);
+
+        // Draw the fill
+        g.setColor(textColor);
+        g.fill(textShape);
+    }
+}
