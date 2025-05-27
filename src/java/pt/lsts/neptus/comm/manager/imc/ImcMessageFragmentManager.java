654ca619e4d18764c2f01999b368d6f122e951db
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-05-27 11:05:49 +0100

    comm/manager/imc/ImcMessageFragmentManager: Added more debug info for fragment reception.

--- src/java/pt/lsts/neptus/comm/manager/imc/ImcMessageFragmentManager.java
+++ src/java/pt/lsts/neptus/comm/manager/imc/ImcMessageFragmentManager.java
@@ -234,6 +234,12 @@ public class ImcMessageFragmentManager {
         if (fragment == null || fragment.getSrc() == GeneralPreferences.imcCcuId.intValue())
             return;
 
+        System.out.println("Received " + ((fragment.getUid() <= 0 || fragment.getSrc() <= 0) ? "invalid " : "")
+                + "fragment: " + fragment.getUid() + " from system " + fragment.getSrc());
+        NeptusLog.pub().warn("Received {}fragment: {} from system {}",
+                (fragment.getUid() <= 0 || fragment.getSrc() <= 0) ? "invalid " : "",
+                fragment.getUid(), fragment.getSrc());
+
         if (fragment.getUid() <= 0 || fragment.getSrc() <= 0) {
             return; // Invalid fragment
         }
