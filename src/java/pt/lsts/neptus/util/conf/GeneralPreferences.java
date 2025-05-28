6cf5893c773f43903fd28285da32e1403c4b2b3c
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-05-28 17:13:16 +0100

    comm/iridium/IridiumManager: Adding configurable time window to get messages.
    # Conflicts:
    #	src/java/pt/lsts/neptus/util/conf/GeneralPreferences.java

--- src/java/pt/lsts/neptus/util/conf/GeneralPreferences.java
+++ src/java/pt/lsts/neptus/util/conf/GeneralPreferences.java
@@ -251,6 +251,21 @@ public class GeneralPreferences implements PropertiesProvider {
             units = "minutes")
     public static double iridiumMessengerPollMinutes = 5;
 
+    @NeptusProperty(name = "Iridium Messenger Poll Window Hours", category="Iridium Communications", userLevel = LEVEL.REGULAR,
+            description = "Iridium messaging poll window in hours. Valid values between 0.17 (~10m) and 24. Doesn't need restart to apply",
+            units = "minutes")
+    public static double iridiumMessengerPollWindowHours = 1;
+
+    @NeptusProperty(name = "Minutes To Dump All Fragments", category="Fragments Communications", userLevel = LEVEL.REGULAR,
+            description = "Minutes to dump all fragments. Valid values are positive values between 10 and 240. Doesn't need restart to apply",
+            units = "minutes")
+    public static long minutesToDumpAllFragments = 60;
+
+    @NeptusProperty(name = "Minutes To Request Missing Received Fragments", category="Fragments Communications", userLevel = LEVEL.REGULAR,
+            description = "Minutes to request missing received fragments. Valid values are positive values between 1 and 60. Doesn't need restart to apply",
+            units = "minutes")
+    public static long minutesToRequestMissingReceivedFragments = 10;
+
     // -------------------------------------------------------------------------
     
     @NeptusProperty(name = "Maximum Size of Plan Name For Acoustics", category="Plan", userLevel = LEVEL.ADVANCED,
@@ -413,6 +428,18 @@ public class GeneralPreferences implements PropertiesProvider {
 
     public static String validateIridiumMessengerPoolMinutes(double value) {
         return new DoubleMinMaxValidator(0.17, 30).validate(value);
+    }
+
+    public static String validateMinutesToDumpAllFragments(long value) {
+        return new LongMinMaxValidator(10, 240).validate(value);
+    }
+
+    public static String validateMinutesToRequestMissingReceivedFragments(long value) {
+        return new LongMinMaxValidator(1, 60).validate(value);
+    }
+
+    public static String validateIridiumMessengerPollWindowHours(double value) {
+        return new DoubleMinMaxValidator(0.17, 24).validate(value);
     }
 
     // -------------------------------------------------------------------------
