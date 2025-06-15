4f215762939a530ef5b31fab857f9cb25216fb2a
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-06-15 12:06:05 +0100

    plugins/alliance/AisContact: Removed debug system out.

--- plugins-dev/alliance/src/java/pt/lsts/neptus/plugins/alliance/AisContact.java
+++ plugins-dev/alliance/src/java/pt/lsts/neptus/plugins/alliance/AisContact.java
@@ -61,7 +61,7 @@ public class AisContact {
     }
     
     public AisContact(int mmsi) {
-        System.out.println("Created AIS contact for mmsi "+mmsi);
+        // System.out.println("Created AIS contact for mmsi "+mmsi);
         this.mmsi = mmsi;
         lastUpdate = System.currentTimeMillis();
     }
