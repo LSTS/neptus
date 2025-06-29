a595a4a995cc78dc559a4ab527f366d2ac5c8c69
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-06-29 11:06:55 +0100

    comm/iridium/HubIridiumMessenger: Tweak to show M1 or M2 for modem IMEI.

--- src/java/pt/lsts/neptus/comm/iridium/HubIridiumMessenger.java
+++ src/java/pt/lsts/neptus/comm/iridium/HubIridiumMessenger.java
@@ -316,13 +316,16 @@ public class HubIridiumMessenger implements IridiumMessenger {
                 String lastSeen = iridiumArgs.getLastSeenImei();
                 iridiumArgs.setLastSeenImei(imei, date);
                 String lastSeenNew = iridiumArgs.getLastSeenImei();
+                String imei0 = iridiumArgs.getImei();
                 if (!lastSeen.equals(lastSeenNew)) {
-                    NeptusLog.pub().info("Updated vehicle {} with last seen IMEI {} from " +
-                                    "the old {} with date {}", veh.getId(), lastSeenNew, lastSeen, date);
+                    NeptusLog.pub().info("Updated vehicle {} to last seen IMEI {} {} from " +
+                                    "the old {} with date {}", veh.getId(), imei0.equals(lastSeenNew) ? "M1" : "M2",
+                                    lastSeenNew, lastSeen, date);
                     NeptusEvents.post(Notification.warning(
                             I18n.textf("Iridium IMEI Change for %s", veh.getId()),
-                            I18n.textf("Updated vehicle %vehicle with last seen IMEI %newImei from " +
-                                    "the old %oldImei with date %date", veh.getId(), lastSeenNew, lastSeen, date)));
+                            I18n.textf("Updated vehicle %vehicle to last seen IMEI %imeiOrdinal %newImei from " +
+                                    "the old %oldImei with date %date", veh.getId(), imei0.equals(lastSeenNew) ? "M1" : "M2",
+                                    lastSeenNew, lastSeen, date)));
                 }
             }
         }
