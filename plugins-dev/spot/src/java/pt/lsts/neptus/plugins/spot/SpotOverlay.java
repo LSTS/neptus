84b316f91f487e3bb8458a90561f508dacd9e108
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-07-08 17:42:33 +0100

    plugins/spot/SpotOverlay: Add error protection.

--- plugins-dev/spot/src/java/pt/lsts/neptus/plugins/spot/SpotOverlay.java
+++ plugins-dev/spot/src/java/pt/lsts/neptus/plugins/spot/SpotOverlay.java
@@ -205,95 +205,101 @@ public class SpotOverlay extends SimpleRendererInteraction implements IPeriodicU
             return;
         }
 
-        Graphics2D g2 = (Graphics2D) g.create();
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // For each spot paint all the known positions with dots in each position and a path connecting them
-        for (Spot spot : spotsOnMap) {
-            if (!spotIDsToShow.isEmpty() && spotIDsToShow.stream().noneMatch(
-                    s -> spotIDsCaseSensitive ? spot.getName().equals(s) : spot.getName().equalsIgnoreCase(s))) {
-                continue;
-            }
-            LocationType spotLoc = spot.getLastLocation();
-            if (spotLoc == null) {
-                continue;
-            }
-            Point2D pt = renderer.getScreenPosition(spotLoc);
-            double xScreenPos = pt.getX();
-            double yScreenPos = pt.getY();
-            g.translate(xScreenPos, yScreenPos);
-
-            boolean hasTime = false;
-            if (showNames) {
-                g.setColor(Color.BLACK);
-                g.drawString(spot.getName(), 7, 1);
-                g.setColor(COLOR_SPOT);
-                g.drawString(spot.getName(), 6, 0);
-
-                Long timestampMillis = spot.getLastLocationTimestampMillis();
-                if (timestampMillis != null) {
-                    hasTime = true;
-                    String dateStr = new Date(timestampMillis).toString();
-                    g.setColor(Color.BLACK);
-                    g.drawString(dateStr, 7, 13);
-                    g.setColor(COLOR_SPOT);
-                    g.drawString(dateStr, 6, 12);
+        try {
+            Graphics2D g2 = (Graphics2D) g.create();
+            g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            // For each spot paint all the known positions with dots in each position and a path connecting them
+            for (Spot spot : spotsOnMap) {
+                if (!spotIDsToShow.isEmpty() && spotIDsToShow.stream().noneMatch(
+                        s -> spotIDsCaseSensitive ? spot.getName().equals(s) : spot.getName().equalsIgnoreCase(s))) {
+                    continue;
                 }
-            }
-
-            double speedMps = spot.getSpeed();
-            if (speedMps != -1) {
-                if (showSpeedValue) {
-                    SpeedType.Units speedUnits = GeneralPreferences.speedUnits;
-                    Pair<Double, SpeedType.Units> convSpeed = UnitsUtil.convertSpeed(speedMps, speedUnits);
-                    String speedTxt = GuiUtils.getNeptusDecimalFormat(1).format(convSpeed.getLeft()) + " " + convSpeed.getRight();
-                    g.setColor(Color.BLACK);
-                    int offset = hasTime ? 12 : 0;
-                    g.drawString(speedTxt, 7, 13 + offset);
-                    g.setColor(COLOR_SPOT);
-                    g.drawString(speedTxt, 6, 12 + offset);
+                LocationType spotLoc = spot.getLastLocation();
+                if (spotLoc == null) {
+                    continue;
                 }
-            }
-            g.translate(-xScreenPos, -yScreenPos);
-            List<LocationType> lastLocations = spot.getLastLocations();
-            //List<Long> lastLocationsTimestampMillis = spot.getLastLocationsTimestampMillis();
-            LocationType location;
-            //Long locationTimestampMillis;
-            GeneralPath spotPath = new GeneralPath();
-            float shadeOfGreyInc = 0.05f;
-            float shadeOfGrey = (lastLocations.size() * shadeOfGreyInc);
-            float min;
-            if (!lastLocations.isEmpty()) {
-                location = lastLocations.get(0);
-                //locationTimestampMillis = lastLocationsTimestampMillis.get(0);
-                pt = renderer.getScreenPosition(location);
-                spotPath.moveTo(pt.getX(), pt.getY());
-                min = 1 - Math.min(shadeOfGrey, 0.8f);
-                g2.setColor(new Color(min, min, min));
-                g2.fillOval((int) (pt.getX() - 4), (int) (pt.getY() - 4), 8, 8);
+                Point2D pt = renderer.getScreenPosition(spotLoc);
+                double xScreenPos = pt.getX();
+                double yScreenPos = pt.getY();
+                g.translate(xScreenPos, yScreenPos);
 
-                for (int i = 1; i < lastLocations.size(); i++) {
-                    location = lastLocations.get(i);
-                    //locationTimestampMillis = lastLocationsTimestampMillis.get(i);
+                boolean hasTime = false;
+                if (showNames) {
+                    g.setColor(Color.BLACK);
+                    g.drawString(spot.getName(), 7, 1);
+                    g.setColor(COLOR_SPOT);
+                    g.drawString(spot.getName(), 6, 0);
+
+                    Long timestampMillis = spot.getLastLocationTimestampMillis();
+                    if (timestampMillis != null) {
+                        hasTime = true;
+                        String dateStr = new Date(timestampMillis).toString();
+                        g.setColor(Color.BLACK);
+                        g.drawString(dateStr, 7, 13);
+                        g.setColor(COLOR_SPOT);
+                        g.drawString(dateStr, 6, 12);
+                    }
+                }
+
+                double speedMps = spot.getSpeed();
+                if (speedMps != -1) {
+                    if (showSpeedValue) {
+                        SpeedType.Units speedUnits = GeneralPreferences.speedUnits;
+                        Pair<Double, SpeedType.Units> convSpeed = UnitsUtil.convertSpeed(speedMps, speedUnits);
+                        String speedTxt = GuiUtils.getNeptusDecimalFormat(1).format(convSpeed.getLeft()) + " " + convSpeed.getRight();
+                        g.setColor(Color.BLACK);
+                        int offset = hasTime ? 12 : 0;
+                        g.drawString(speedTxt, 7, 13 + offset);
+                        g.setColor(COLOR_SPOT);
+                        g.drawString(speedTxt, 6, 12 + offset);
+                    }
+                }
+                g.translate(-xScreenPos, -yScreenPos);
+                List<LocationType> lastLocations = spot.getLastLocations();
+                //List<Long> lastLocationsTimestampMillis = spot.getLastLocationsTimestampMillis();
+                LocationType location;
+                //Long locationTimestampMillis;
+                GeneralPath spotPath = new GeneralPath();
+                float shadeOfGreyInc = 0.05f;
+                float shadeOfGrey = (lastLocations.size() * shadeOfGreyInc);
+                float min;
+                if (!lastLocations.isEmpty()) {
+                    location = lastLocations.get(0);
+                    //locationTimestampMillis = lastLocationsTimestampMillis.get(0);
                     pt = renderer.getScreenPosition(location);
-                    spotPath.lineTo(pt.getX(), pt.getY());
+                    spotPath.moveTo(pt.getX(), pt.getY());
                     min = 1 - Math.min(shadeOfGrey, 0.8f);
                     g2.setColor(new Color(min, min, min));
                     g2.fillOval((int) (pt.getX() - 4), (int) (pt.getY() - 4), 8, 8);
-                    shadeOfGrey -= shadeOfGreyInc;
+
+                    for (int i = 1; i < lastLocations.size(); i++) {
+                        location = lastLocations.get(i);
+                        //locationTimestampMillis = lastLocationsTimestampMillis.get(i);
+                        pt = renderer.getScreenPosition(location);
+                        spotPath.lineTo(pt.getX(), pt.getY());
+                        min = 1 - Math.min(shadeOfGrey, 0.8f);
+                        g2.setColor(new Color(min, min, min));
+                        g2.fillOval((int) (pt.getX() - 4), (int) (pt.getY() - 4), 8, 8);
+                        shadeOfGrey -= shadeOfGreyInc;
+                    }
+
+                    g2.setStroke(new BasicStroke(3f));
+                    g2.setColor(COLOR_SPOT_A128);
+                    g2.draw(spotPath);
+
+                    g2.setStroke(new BasicStroke(1f));
+                    g2.setColor(Color.darkGray);
+                    g2.drawOval((int) (pt.getX() - 5), (int) (pt.getY() - 5), 9, 9);
+                    g2.setColor(new Color(255, 148, 143, 128));
+                    g2.fillOval((int) (pt.getX() - 5), (int) (pt.getY() - 5), 10, 10);
+                    g2.setColor(Color.darkGray);
+                    g2.fillOval((int) (pt.getX() - 2), (int) (pt.getY() - 2), 4, 4  );
                 }
-
-                g2.setStroke(new BasicStroke(3f));
-                g2.setColor(COLOR_SPOT_A128);
-                g2.draw(spotPath);
-
-                g2.setStroke(new BasicStroke(1f));
-                g2.setColor(Color.darkGray);
-                g2.drawOval((int) (pt.getX() - 5), (int) (pt.getY() - 5), 9, 9);
-                g2.setColor(new Color(255, 148, 143, 128));
-                g2.fillOval((int) (pt.getX() - 5), (int) (pt.getY() - 5), 10, 10);
-                g2.setColor(Color.darkGray);
-                g2.fillOval((int) (pt.getX() - 2), (int) (pt.getY() - 2), 4, 4  );
             }
+        }
+        catch (Exception e) {
+            NeptusLog.pub().error("Error while painting SPOT overlay", e);
+            return;
         }
     }
 
