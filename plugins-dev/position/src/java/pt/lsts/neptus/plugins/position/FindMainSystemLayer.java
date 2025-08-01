70f0f399048b25d6ed2c574b8d5cb0f4bba96c1c
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-08-01 14:26:25 +0100

    plugins/position/FindMainSystemLayer: Added nautical miles info as well.

--- plugins-dev/position/src/java/pt/lsts/neptus/plugins/position/FindMainSystemLayer.java
+++ plugins-dev/position/src/java/pt/lsts/neptus/plugins/position/FindMainSystemLayer.java
@@ -186,12 +186,20 @@ public class FindMainSystemLayer extends ConsoleLayer {
         else {
             txt.append(absHeadingRadsToLookOrientation.getAbbrev()).append("<br/>");
             txt.append(Math.round(Math.toDegrees(absHeadingRadsToLook))).append("\u00B0<br/>");
+            double absDistanceToLookNMiles = absDistanceToLook / 1852.0;
             int dc = 0;
+            int dcNM = 0;
             if (absDistanceToLook >= 1E3)
                 dc = 2;
             if (absDistanceToLook >= 100E3)
                 dc = 1;
+            if (absDistanceToLookNMiles >= 1E3)
+                dcNM = 2;
+            if (absDistanceToLookNMiles >= 100E3)
+                dcNM = 1;
             txt.append(MathMiscUtils.parseToEngineeringNotation(absDistanceToLook, dc)).append("m");
+            if (absDistanceToLookNMiles >= 1)
+                txt.append("<br/>").append(MathMiscUtils.parseToEngineeringNotation(absDistanceToLookNMiles, dcNM)).append("NM");
         }
         txt.append("</b></div></html>");
         toDraw.setText(txt.toString());
