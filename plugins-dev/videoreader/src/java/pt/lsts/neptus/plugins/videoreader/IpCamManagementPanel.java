e681f693d7a0482fe33615a2f12023adbcb621ab
Paulo Dias <paulo.sousa.dias@gmail.com>
2025-11-12 13:37:47 +0000

    plugins/videoreader/IpCamManagementPanel: React to ESC to close the dialog.

--- plugins-dev/videoreader/src/java/pt/lsts/neptus/plugins/videoreader/IpCamManagementPanel.java
+++ plugins-dev/videoreader/src/java/pt/lsts/neptus/plugins/videoreader/IpCamManagementPanel.java
@@ -36,6 +36,7 @@ import foxtrot.AsyncWorker;
 import net.miginfocom.swing.MigLayout;
 import pt.lsts.neptus.NeptusLog;
 import pt.lsts.neptus.i18n.I18n;
+import pt.lsts.neptus.util.GuiUtils;
 import pt.lsts.neptus.util.ImageUtils;
 import pt.lsts.neptus.util.conf.ConfigFetch;
 
@@ -107,6 +108,7 @@ public class IpCamManagementPanel extends JPanel {
         ipCamPing.setModalityType(Dialog.ModalityType.DOCUMENT_MODAL);
         ipCamPing.setSize(440, 200);
         ipCamPing.setLocationRelativeTo(videoReader);
+        GuiUtils.reactEscapeKeyPress(ipCamPing);
 
         ImageIcon imgIPCam = ImageUtils.createImageIcon("images/menus/camera.png");
         if (imgIPCam != null) {
